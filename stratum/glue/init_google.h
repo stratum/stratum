40a3252de45d9eeeb14973be428013ab20c566e5
Brian O'Connor <bocon@opennetworking.org>
2018-06-11 20:27:38 -0700

    Updating glue/init_google to build using gflags

--- stratum/glue/init_google.h
+++ stratum/glue/init_google.h
@@ -18,13 +18,17 @@
 #ifndef STRATUM_GLUE_INIT_GOOGLE_H_
 #define STRATUM_GLUE_INIT_GOOGLE_H_
 
-#include "base/init_google.h"  // IWYU pragma: export
+//TODO this is not required for open source
+//#include "base/init_google.h"  // IWYU pragma: export
 
 // InitGoogle is not defined in portable base, so we instead emulate its
 // behavior.
 #if !GOOGLE_BASE_HAS_INITGOOGLE
 
 #include "gflags/gflags.h"
+
+//TODO need to define transformation or comment this out on Google's side
+using gflags::ParseCommandLineFlags;
 
 inline void InitGoogle(const char *usage, int *argc, char ***argv,
                        bool remove_flags) {
