7165a9cf17d57d3b24dabe2d739ca4852eee4c2a
Yi Tseng <yi@opennetworking.org>
2018-12-10 09:41:32 -0800

    Remove duplicate code from YangParseTreePaths
    Issue: #84

--- stratum/hal/lib/common/yang_parse_tree_paths.cc
+++ stratum/hal/lib/common/yang_parse_tree_paths.cc
@@ -2212,39 +2212,6 @@ void YangParseTreePaths::AddSubtreeAllInterfaces(YangParseTree* tree) {
                     status, YangParseTreePaths::SendEndOfSeriesMessage(stream));
                 return status;
               });
-  // Add support for "/interfaces/interface/...".
-  tree->AddNode(GetPath("interfaces")("interface")("...")())
-      ->SetOnChangeRegistration(
-          [tree](const EventHandlerRecordPtr& record)
-              EXCLUSIVE_LOCKS_REQUIRED(tree->root_access_lock_) {
-                // Subscribing to a wildcard node means that all matching nodes
-                // have to be registered for received events.
-                auto status = tree->PerformActionForAllNonWildcardNodes(
-                    GetPath("interfaces")("interface")(), gnmi::Path(),
-                    [&record](const TreeNode& node) {
-                      return node.DoOnChangeRegistration(record);
-                    });
-                return status;
-              })
-      ->SetOnChangeHandler(
-          [tree](const GnmiEvent& event, const ::gnmi::Path& path,
-                 GnmiSubscribeStream* stream) { return ::util::OkStatus(); })
-      ->SetOnPollHandler(
-          [tree](const GnmiEvent& event, const ::gnmi::Path& path,
-                 GnmiSubscribeStream* stream)
-              EXCLUSIVE_LOCKS_REQUIRED(tree->root_access_lock_) {
-                // Polling a wildcard node means that all matching nodes have to
-                // be polled.
-                auto status = tree->PerformActionForAllNonWildcardNodes(
-                    GetPath("interfaces")("interface")(), gnmi::Path(),
-                    [&event, &stream](const TreeNode& node) {
-                      return (node.GetOnPollHandler())(event, stream);
-                    });
-                // Notify the client that all nodes have been processed.
-                APPEND_STATUS_IF_ERROR(
-                    status, YangParseTreePaths::SendEndOfSeriesMessage(stream));
-                return status;
-              });
 }
 
 void YangParseTreePaths::AddRoot(YangParseTree* tree) {
