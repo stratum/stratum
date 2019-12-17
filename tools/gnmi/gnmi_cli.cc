65a803ee4763f22a084ee8dd22a8ddc74eebe4b1
Yi Tseng <yi@opennetworking.org>
2019-12-17 15:50:45 -0800

    minor fixes

--- tools/gnmi/gnmi_cli.cc
+++ tools/gnmi/gnmi_cli.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <csignal>
 #include <grpcpp/grpcpp.h>
 #include <iostream>
 #include <memory>
@@ -21,7 +22,7 @@
 
 #define STRIP_FLAG_HELP 1  // remove additional flag help text from gflag
 #include "gflags/gflags.h"
-#include "github.com/openconfig/gnmi/proto/gnmi/gnmi.grpc.pb.h"
+#include "gnmi/gnmi.grpc.pb.h"
 
 const char kUsage[] =
 R"USAGE(usage: gnmi-cli.py [-h] [-grpc_addr GRPC_ADDR] [-bool_val BOOL_VAL]
@@ -32,17 +33,20 @@ R"USAGE(usage: gnmi-cli.py [-h] [-grpc_addr GRPC_ADDR] [-bool_val BOOL_VAL]
 Basic gNMI CLI
 
 positional arguments:
-  {get,set,sub}         gNMI command
-  path                  gNMI path
+  {get,set,sub,cap}         gNMI command
+  path                      gNMI path
 
 optional arguments:
   --help            show this help message and exit
-  -grpc_addr GRPC_ADDR    gNMI server address
-  -bool_val BOOL_VAL      [SetRequest only] Set boolean value
-  -int_val INT_VAL        [SetRequest only] Set int value (64-bit)
-  -uint_val UINT_VAL      [SetRequest only] Set uint value (64-bit)
-  -string_val STRING_VAL  [SetRequest only] Set string value
-  -float_val FLOAT_VAL    [SetRequest only] Set float value)USAGE";
+  --grpc_addr GRPC_ADDR    gNMI server address
+  --bool_val BOOL_VAL      [SetRequest only] Set boolean value
+  --int_val INT_VAL        [SetRequest only] Set int value (64-bit)
+  --uint_val UINT_VAL      [SetRequest only] Set uint value (64-bit)
+  --string_val STRING_VAL  [SetRequest only] Set string value
+  --float_val FLOAT_VAL    [SetRequest only] Set float value
+  --interval INTERVAL      [Sample subscribe only] Sample subscribe poll interval in ms
+  --replace                [SetRequest only] Use replace instead of update
+)USAGE";
 
 #define PRINT_MSG(msg, prompt) \
   std::cout << prompt << std::endl; \
@@ -64,6 +68,9 @@ DEFINE_string(int_val, "", "Integer value to be set (64-bit)");
 DEFINE_string(uint_val, "", "Unsigned integer value to be set (64-bit)");
 DEFINE_string(string_val, "", "String value to be set");
 DEFINE_string(float_val, "", "Floating point value to be set");
+
+DEFINE_uint64(interval, 5000, "Subscribe poll interval in ms");
+DEFINE_bool(replace, false, "Use replace instead of update");
 
 namespace stratum {
 namespace tools {
@@ -106,7 +113,12 @@ void build_gnmi_path(std::string path_str, ::gnmi::Path* path) {
 
 ::gnmi::SetRequest build_gnmi_set_req(std::string path) {
   ::gnmi::SetRequest req;
-  auto* update = req.add_update();
+  ::gnmi::Update* update;
+  if (FLAGS_replace) {
+    update = req.add_replace();
+  } else {
+    update = req.add_update();
+  }
   build_gnmi_path(path, update->mutable_path());
   if (!FLAGS_bool_val.empty()) {
     update->mutable_val()->set_bool_val(str_to_bool(FLAGS_bool_val));
@@ -124,7 +136,14 @@ void build_gnmi_path(std::string path_str, ::gnmi::Path* path) {
   return req;
 }
 
-::gnmi::SubscribeRequest build_gnmi_sub_req(std::string path) {
+::gnmi::SetRequest build_gnmi_del_req(std::string path) {
+  ::gnmi::SetRequest req;
+  auto* del = req.add_delete_();
+  build_gnmi_path(path, del);
+  return req;
+}
+
+::gnmi::SubscribeRequest build_gnmi_sub_onchange_req(std::string path) {
   ::gnmi::SubscribeRequest sub_req;
   auto* sub_list = sub_req.mutable_subscribe();
   sub_list->set_mode(::gnmi::SubscriptionList::STREAM);
@@ -135,12 +154,29 @@ void build_gnmi_path(std::string path_str, ::gnmi::Path* path) {
   return sub_req;
 }
 
+::gnmi::SubscribeRequest
+build_gnmi_sub_sample_req(std::string path,
+                          ::google::protobuf::uint64 interval) {
+  ::gnmi::SubscribeRequest sub_req;
+  auto* sub_list = sub_req.mutable_subscribe();
+  sub_list->set_mode(::gnmi::SubscriptionList::STREAM);
+  sub_list->set_updates_only(true);
+  auto* sub = sub_list->add_subscription();
+  sub->set_mode(::gnmi::SAMPLE);
+  sub->set_sample_interval(interval);
+  build_gnmi_path(path, sub->mutable_path());
+  return sub_req;
+}
+
+::grpc::ClientReaderWriterInterface
+    <::gnmi::SubscribeRequest, ::gnmi::SubscribeResponse>* stream_reader_writer;
+
 int Main(int argc, char** argv) {
   if (argc < 3) {
     std::cout << kUsage << std::endl;
     return -1;
   }
-  ::grpc::ClientContext cxt;
+  ::grpc::ClientContext ctx;
   std::string cmd = std::string(argv[1]);
   std::string path = std::string(argv[2]);
   auto channel = ::grpc::CreateChannel(FLAGS_grpc_addr,
@@ -152,17 +188,23 @@ int Main(int argc, char** argv) {
     ::gnmi::GetRequest req = build_gnmi_get_req(path);
     PRINT_MSG(req, "REQUEST")
     ::gnmi::GetResponse resp;
-    status = stub->Get(&cxt, req, &resp);
+    status = stub->Get(&ctx, req, &resp);
     CHECK_AND_PRINT_RESP(status, resp)
   } else if (cmd == "set") {
     ::gnmi::SetRequest req = build_gnmi_set_req(path);
     PRINT_MSG(req, "REQUEST")
     ::gnmi::SetResponse resp;
-    status = stub->Set(&cxt, req, &resp);
+    status = stub->Set(&ctx, req, &resp);
     CHECK_AND_PRINT_RESP(status, resp)
-  } else if (cmd == "sub") {
-    auto stream_reader_writer = stub->Subscribe(&cxt);
-    ::gnmi::SubscribeRequest req = build_gnmi_sub_req(path);
+  } else if (cmd == "del") {
+    ::gnmi::SetRequest req = build_gnmi_del_req(path);
+    PRINT_MSG(req, "REQUEST")
+    ::gnmi::SetResponse resp;
+    status = stub->Set(&ctx, req, &resp);
+    CHECK_AND_PRINT_RESP(status, resp)
+  } else if (cmd == "sub-onchange") {
+    stream_reader_writer = stub->Subscribe(&ctx).get();
+    ::gnmi::SubscribeRequest req = build_gnmi_sub_onchange_req(path);
     PRINT_MSG(req, "REQUEST")
     if (!stream_reader_writer->Write(req)) {
       std::cout << "Can not write request" << std::endl;
@@ -173,10 +215,38 @@ int Main(int argc, char** argv) {
     }
     status = stream_reader_writer->Finish();
     LOG_IF_NOT_OK(status);
+  } else if (cmd == "sub-sample") {
+    stream_reader_writer = stub->Subscribe(&ctx).get();
+    ::gnmi::SubscribeRequest req =
+        build_gnmi_sub_sample_req(path, FLAGS_interval);
+    PRINT_MSG(req, "REQUEST")
+    if (!stream_reader_writer->Write(req)) {
+      std::cout << "Can not write request" << std::endl;
+    }
+    ::gnmi::SubscribeResponse resp;
+    while (stream_reader_writer->Read(&resp)) {
+      PRINT_MSG(resp, "RESPONSE")
+    }
+    status = stream_reader_writer->Finish();
+    LOG_IF_NOT_OK(status);
+  } else if (cmd == "cap") {
+    ::gnmi::CapabilityRequest req;
+    PRINT_MSG(req, "REQUEST")
+    ::gnmi::CapabilityResponse resp;
+    stub->Capabilities(&ctx, req, &resp);
+    CHECK_AND_PRINT_RESP(status, resp)
   } else {
     std::cout << "Unknown command: " << cmd << std::endl;
   }
   return 0;
+}
+
+void HandleSignal(int signal) {
+  // Send empty subscribe request to close the stream.
+  ::gnmi::SubscribeRequest req;
+  if (!stream_reader_writer->Write(req)) {
+    std::cout << "Can not write request" << std::endl;
+  }
 }
 
 }  // namespace gnmi
@@ -186,5 +256,6 @@ int Main(int argc, char** argv) {
 int main(int argc, char** argv) {
   ::gflags::SetUsageMessage(kUsage);
   ::gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::signal(SIGINT, stratum::tools::gnmi::HandleSignal);
   return stratum::tools::gnmi::Main(argc, argv);
 }
