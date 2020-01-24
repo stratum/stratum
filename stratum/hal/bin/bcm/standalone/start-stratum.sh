ccf35596a74eea67af7016b549bb0c4570a1538c
pudelkoM <pudelkoM@users.noreply.github.com>
2020-01-24 15:14:12 -0800

    more fixes

--- stratum/hal/bin/bcm/standalone/start-stratum.sh
+++ stratum/hal/bin/bcm/standalone/start-stratum.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
+# Remove other SDK kernel modules, if present
+rmmod linux_bcm_knet || true
+rmmod linux_user_bde || true
+rmmod linux_kernel_bde || true
+
+# Reinsert SDKLT kernel modules
 rmmod linux_ngknet || true
 rmmod linux_ngbde || true
 insmod linux_ngbde.ko && insmod linux_ngknet.ko
@@ -14,9 +20,9 @@ PLATFORM=$(cat /etc/onl/platform)
     -base_bcm_chassis_map_file=./stratum_configs/${PLATFORM}/base_bcm_chassis_map.pb.txt \
     -chassis_config_file=./stratum_configs/${PLATFORM}/chassis_config.pb.txt \
     -bcm_sdk_config_file=./stratum_configs/${PLATFORM}/SDKLT.yml \
+    -phal_config_path=./stratum_configs/${PLATFORM}/phal_config.pb.txt \
     -bcm_serdes_db_proto_file=./stratum_configs/dummy_serdes_db.pb.txt \
     -bcm_hardware_specs_file=./stratum_configs/bcm_hardware_specs.pb.txt \
-    -phal_config_path=/dev/null
     -forwarding_pipeline_configs_file=/tmp/stratum/pipeline_cfg.pb.txt \
     -write_req_log_file=/tmp/stratum/p4_writes.pb.txt \
     -bcm_sdk_checkpoint_dir=/tmp/bcm_chkpt \
