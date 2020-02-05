c13badfc4c195e2299f536286062f5eb397832d0
pudelkoM <pudelkoM@users.noreply.github.com>
2020-02-04 21:04:39 -0800

    Fix SfpAdapter tests

--- stratum/hal/lib/phal/sfp_adapter_test.cc
+++ stratum/hal/lib/phal/sfp_adapter_test.cc
@@ -15,7 +15,8 @@
 
 #include "stratum/hal/lib/phal/sfp_adapter.h"
 
-#include <functional>
+#include <string>
+#include <utility>
 #include <vector>
 
 #include "absl/memory/memory.h"
@@ -24,8 +25,9 @@
 #include "stratum/glue/status/status.h"
 #include "stratum/glue/status/status_macros.h"
 #include "stratum/glue/status/status_test_util.h"
-// #include "stratum/hal/lib/phal/onlp/onlp_wrapper_mock.h"
+#include "stratum/hal/lib/phal/attribute_database_mock.h"
 #include "stratum/lib/macros.h"
+#include "stratum/lib/utils.h"
 
 DECLARE_int32(onlp_polling_interval_ms);
 
@@ -33,14 +35,12 @@ namespace stratum {
 namespace hal {
 namespace phal {
 
-// TODO(max)
-#if 0
-
 using TransceiverEvent = PhalInterface::TransceiverEvent;
 
 using stratum::test_utils::StatusIs;
 using ::testing::_;
 using ::testing::AllOf;
+using ::testing::ByMove;
 using ::testing::Invoke;
 using ::testing::MockFunction;
 using ::testing::Return;
@@ -48,110 +48,80 @@ using ::testing::StrictMock;
 
 static constexpr int kMaxXcvrEventDepth = 256;
 
-
-class OnlpPhalTest : public ::testing::Test {
+class SfpAdapterTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    onlp_wrapper_mock_ = absl::make_unique<StrictMock<OnlpWrapperMock>>();
-    onlp_sfp_info_t onlp_sfp_info = {};
-    onlp_sfp_info.type = ONLP_SFP_TYPE_QSFP;
-    onlp_sfp_info.hdr.id = ONLP_SFP_ID_CREATE(1);
-    onlp_sfp_info.hdr.status = ONLP_OID_STATUS_FLAG_PRESENT;
-    onlp_sfp_info.sff.module_type = SFF_MODULE_TYPE_40G_BASE_CR4;
-    onlp_sfp_info.sff.sfp_type = SFF_SFP_TYPE_QSFP;
-    snprintf(onlp_sfp_info.sff.vendor, sizeof(onlp_sfp_info.sff.vendor),
-             "sfp-vendor-name");
-    SfpInfo sfp1_info(onlp_sfp_info);
-    onlp_sfp_info.hdr.id = ONLP_SFP_ID_CREATE(2);
-    SfpInfo sfp2_info(onlp_sfp_info);
-
-    std::vector<OnlpOid> oids = {ONLP_SFP_ID_CREATE(1), ONLP_SFP_ID_CREATE(2)};
-
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidList(ONLP_OID_TYPE_FLAG_SFP))
-        .WillRepeatedly(Return(std::vector<OnlpOid>(oids)));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidList(ONLP_OID_TYPE_FLAG_PSU))
-        .WillRepeatedly(Return(std::vector<OnlpOid>({})));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidList(ONLP_OID_TYPE_FLAG_FAN))
-        .WillRepeatedly(Return(std::vector<OnlpOid>({})));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidList(ONLP_OID_TYPE_FLAG_LED))
-        .WillRepeatedly(Return(std::vector<OnlpOid>({})));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidList(ONLP_OID_TYPE_FLAG_THERMAL))
-        .WillRepeatedly(Return(std::vector<OnlpOid>({})));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetSfpInfo(ONLP_SFP_ID_CREATE(1)))
-        .WillRepeatedly(Return(sfp1_info));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetSfpInfo(117440514))
-        .WillRepeatedly(Return(sfp2_info));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidInfo(ONLP_SFP_ID_CREATE(1)))
-        .WillRepeatedly(Return(sfp1_info));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetOidInfo(117440514))
-        .WillRepeatedly(Return(sfp2_info));
-    EXPECT_CALL(*onlp_wrapper_mock_, GetSfpMaxPortNumber())
-        .WillRepeatedly(Return(2));
-    // CreateSingleton calls Initialize()
-    onlp_phal_ = OnlpPhal::CreateSingleton(onlp_wrapper_mock_.get());
-
-    // Wait a bit so that the Onlp event handler can pick up the present state.
-    absl::SleepFor(absl::Milliseconds(FLAGS_onlp_polling_interval_ms + 100));
+    database_ = absl::make_unique<AttributeDatabaseMock>();
+    sfp_adapter_ = absl::make_unique<SfpAdapter>(database_.get());
   }
 
-  void TearDown() override { onlp_phal_->Shutdown(); }
+  std::unique_ptr<AttributeDatabaseMock> database_;
+  std::unique_ptr<SfpAdapter> sfp_adapter_;
 
- protected:
-  int card_ = 1;
-  OnlpPhal* onlp_phal_;
-  std::unique_ptr<StrictMock<OnlpWrapperMock>> onlp_wrapper_mock_;
+  const std::string phaldb_get_response_proto = R"PROTO(
+    cards {
+      ports {
+        physical_port_type: PHYSICAL_PORT_TYPE_SFP_CAGE
+        transceiver {
+          id: 0
+          description: "port-0"
+          hardware_state: HW_STATE_PRESENT
+          media_type: MEDIA_TYPE_SFP
+          connector_type: SFP_TYPE_SFP
+          module_type: SFP_MODULE_TYPE_10G_BASE_CR
+          info {
+            mfg_name: "test_vendor"
+            part_no: "test part #"
+            serial_no: "test1234"
+          }
+        }
+      }
+    }
+  )PROTO";
 };
 
-// TEST_F(OnlpPhalTest, OnlpPhalRegisterAndUnregisterTransceiverEventWriter) {
-//   std::shared_ptr<Channel<TransceiverEvent>> channel =
-//       Channel<TransceiverEvent>::Create(kMaxXcvrEventDepth);
+namespace {
 
-//   // Create and hand-off ChannelWriter to the PhalInterface.
-//   auto writer1 = ChannelWriter<TransceiverEvent>::Create(channel);
-//   auto writer2 = ChannelWriter<TransceiverEvent>::Create(channel);
+TEST_F(SfpAdapterTest, OnlpPhalGetFrontPanelPortInfoSuccess) {
+  auto db_query_mock = absl::make_unique<QueryMock>();
+  auto db_query = db_query_mock.get();
+  EXPECT_CALL(*database_.get(), MakeQuery(_))
+      .WillOnce(Return(ByMove(
+          ::util::StatusOr<std::unique_ptr<Query>>(std::move(db_query_mock)))));
+  auto phaldb_resp = absl::make_unique<PhalDB>();
+  ASSERT_OK(ParseProtoFromString(phaldb_get_response_proto, phaldb_resp.get()));
+  EXPECT_CALL(*db_query, Get())
+      .WillOnce(Return(ByMove(std::move(phaldb_resp))));
+  FrontPanelPortInfo fp_port_info{};
 
-//   // Register writer1
-//   ::util::StatusOr<int> result = onlp_phal_->RegisterTransceiverEventWriter(
-//       std::move(writer1), PhalInterface::kTransceiverEventWriterPriorityMed);
-//   EXPECT_TRUE(result.ok());
-//   int id1 = result.ValueOrDie();
-//   EXPECT_EQ(id1, 1);
-
-//   // Register writer2
-//   result = onlp_phal_->RegisterTransceiverEventWriter(
-//       std::move(writer2), PhalInterface::kTransceiverEventWriterPriorityHigh);
-//   EXPECT_TRUE(result.ok());
-//   int id2 = result.ValueOrDie();
-//   EXPECT_EQ(id2, 2);
-
-//   // Unregister writer1
-//   EXPECT_OK(onlp_phal_->UnregisterTransceiverEventWriter(id1));
-
-//   // Unregister writer2
-//   EXPECT_OK(onlp_phal_->UnregisterTransceiverEventWriter(id2));
-// }
-
-TEST_F(OnlpPhalTest, OnlpPhalGetFrontPanelPortInfo) {
-  // SFP 1
-  FrontPanelPortInfo fp_port_info1{};
-  EXPECT_OK(onlp_phal_->GetFrontPanelPortInfo(card_, 1, &fp_port_info1));
-  EXPECT_EQ(fp_port_info1.physical_port_type(), PHYSICAL_PORT_TYPE_QSFP_CAGE);
-  EXPECT_EQ(fp_port_info1.media_type(), MEDIA_TYPE_QSFP_COPPER);
-  EXPECT_EQ(fp_port_info1.vendor_name(), "sfp-vendor-name");
-  // EXPECT_EQ(fp_port_info1.get_part_number(), 6);
-  // EXPECT_EQ(fp_port_info1.serial_number(), "test_sfp_serial");
-
-  // SFP 2
-  FrontPanelPortInfo fp_port_info2{};
-  EXPECT_OK(onlp_phal_->GetFrontPanelPortInfo(card_, 2, &fp_port_info2));
-  EXPECT_EQ(fp_port_info2.physical_port_type(), PHYSICAL_PORT_TYPE_QSFP_CAGE);
-  EXPECT_EQ(fp_port_info2.media_type(), MEDIA_TYPE_QSFP_COPPER);
-  EXPECT_EQ(fp_port_info2.vendor_name(), "sfp-vendor-name");
-  // EXPECT_EQ(fp_port_info2.get_part_number(), 6);
-  // EXPECT_EQ(fp_port_info2.serial_number(), "sfp_serial_222");
+  EXPECT_OK(sfp_adapter_->GetFrontPanelPortInfo(1, 1, &fp_port_info));
+  EXPECT_EQ(fp_port_info.physical_port_type(), PHYSICAL_PORT_TYPE_SFP_CAGE);
+  EXPECT_EQ(fp_port_info.media_type(), MEDIA_TYPE_SFP);
+  EXPECT_EQ(fp_port_info.vendor_name(), "test_vendor");
+  EXPECT_EQ(fp_port_info.part_number(), "test part #");
+  EXPECT_EQ(fp_port_info.serial_number(), "test1234");
 }
 
-#endif
+TEST_F(SfpAdapterTest, OnlpPhalGetFrontPanelPortInfoFailureInvalidPort) {
+  auto db_query_mock = absl::make_unique<QueryMock>();
+  auto db_query = db_query_mock.get();
+  EXPECT_CALL(*database_.get(), MakeQuery(_))
+      .WillOnce(Return(ByMove(
+          ::util::StatusOr<std::unique_ptr<Query>>(std::move(db_query_mock)))));
+  auto phaldb_resp = absl::make_unique<PhalDB>();
+  ASSERT_OK(ParseProtoFromString(phaldb_get_response_proto, phaldb_resp.get()));
+  EXPECT_CALL(*db_query, Get())
+      .WillOnce(Return(ByMove(std::move(phaldb_resp))));
+  FrontPanelPortInfo fp_port_info{};
+
+  auto status = sfp_adapter_->GetFrontPanelPortInfo(1, 2, &fp_port_info);
+  EXPECT_FALSE(status.ok()) << status;
+}
+
+// TODO(max): add tests
+TEST_F(SfpAdapterTest, OnlpPhalRegisterAndUnregisterTransceiverEventWriter) {}
+
+}  // namespace
 
 }  // namespace phal
 }  // namespace hal
