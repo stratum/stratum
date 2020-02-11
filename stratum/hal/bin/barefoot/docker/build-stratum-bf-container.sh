7eb25fc478f84bc32f779b827875c76ea26efede
Yi Tseng <yi@opennetworking.org>
2020-02-11 14:35:26 -0800

    Include Stratum without onlp to container

--- stratum/hal/bin/barefoot/docker/build-stratum-bf-container.sh
+++ stratum/hal/bin/barefoot/docker/build-stratum-bf-container.sh
@@ -18,28 +18,13 @@
 STRATUM_ROOT=${STRATUM_ROOT:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../.." >/dev/null 2>&1 && pwd )"}
 JOBS=${JOBS:-4}
 
-
-
-if [ -z $TM ]; then
-  if [ "$#" -ne 2 ]; then
-      echo "Usage: $0 SDE_TAR KERNEL_HEADERS_TAR"
-      exit 1
-  fi
-
-  docker build -t stratumproject/stratum-bf \
-                 --build-arg JOBS=$JOBS \
-                 --build-arg SDE_TAR=$1 \
-                 --build-arg KERNEL_HEADERS_TAR=$2 \
-                 -f $STRATUM_ROOT/stratum/hal/bin/barefoot/docker/Dockerfile $STRATUM_ROOT
-else
-  # For Tofino Model, no kernel module and ONLP support
-  if [ "$#" -ne 1 ]; then
-      echo "Usage: $0 SDE_TAR"
-      exit 1
-  fi
-
-  docker build -t stratumproject/stratum-bf \
-                 --build-arg JOBS=$JOBS \
-                 --build-arg SDE_TAR=$1 \
-                 -f $STRATUM_ROOT/stratum/hal/bin/barefoot/docker/Dockerfile.tm $STRATUM_ROOT
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 SDE_TAR KERNEL_HEADERS_TAR"
+    exit 1
 fi
+
+docker build -t stratumproject/stratum-bf \
+               --build-arg JOBS=$JOBS \
+               --build-arg SDE_TAR=$1 \
+               --build-arg KERNEL_HEADERS_TAR=$2 \
+               -f $STRATUM_ROOT/stratum/hal/bin/barefoot/docker/Dockerfile $STRATUM_ROOT
