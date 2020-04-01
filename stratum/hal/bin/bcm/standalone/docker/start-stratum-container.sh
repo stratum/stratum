fb44dd99daedf5b55e35b1ca16615ee309a37b9f
pudelkoM <pudelkoM@users.noreply.github.com>
2020-04-01 14:30:58 -0700

    Formatting and style cleanup

--- stratum/hal/bin/bcm/standalone/docker/start-stratum-container.sh
+++ stratum/hal/bin/bcm/standalone/docker/start-stratum-container.sh
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-set -e
-set -x
+
+set -ex
 
 ONLP_MOUNT=$(ls /lib/**/libonlp* | awk '{print "-v " $1 ":" $1 " " }')
 
