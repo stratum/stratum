b4058beddf1b5a0852e9b0609b2349044683e78a
Yi Tseng <yi@opennetworking.org>
2020-04-08 00:34:53 +0800

    resolve comments

--- stratum/hal/lib/barefoot/bf_pal_wrapper.cc
+++ stratum/hal/lib/barefoot/bf_pal_wrapper.cc
@@ -203,7 +203,7 @@ LoopbackModeToBf(LoopbackState loopback_mode) {
       return BF_LPBK_MAC_NEAR;
     default:
       RETURN_ERROR(ERR_INVALID_PARAM) << "Unsupported loopback mode: "
-          << loopback_mode;
+          << LoopbackState_Name(loopback_mode) << ".";
   }
 }
 
@@ -292,7 +292,7 @@ bool BFPalWrapper::PortIsValid(int unit, uint32 port_id) {
 ::util::Status BFPalWrapper::PortLoopbackModeSet(int unit, uint32 port_id,
                                                  LoopbackState loopback_mode) {
   if (loopback_mode == LOOPBACK_STATE_UNKNOWN) {
-    // Do nothing if we try to set loopback mode to default one (UNKNOWN).
+    // Do nothing if we try to set loopback mode to the default one (UNKNOWN).
     return ::util::OkStatus();
   }
   ASSIGN_OR_RETURN(bf_loopback_mode_e lp_mode, LoopbackModeToBf(loopback_mode));
@@ -301,8 +301,9 @@ bool BFPalWrapper::PortIsValid(int unit, uint32 port_id) {
                                     static_cast<bf_dev_port_t>(port_id),
                                     lp_mode);
   if (bf_status != BF_SUCCESS) {
-    return MAKE_ERROR(ERR_INTERNAL) << "Error when setting loopback mode on dev"
-                                    << unit << " port " << port_id;
+    return MAKE_ERROR(ERR_INTERNAL)
+        << "Error when setting loopback mode on dev " << unit << " port "
+        << port_id << ".";
   }
   return ::util::OkStatus();
 }
