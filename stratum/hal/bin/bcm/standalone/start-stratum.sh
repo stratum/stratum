b454408c606aa9a94bc69c367b9d5599cf00b9a2
pudelkoM <pudelkoM@users.noreply.github.com>
2020-04-20 13:07:44 -0700

    WIP

--- stratum/hal/bin/bcm/standalone/start-stratum.sh
+++ stratum/hal/bin/bcm/standalone/start-stratum.sh
@@ -19,19 +19,21 @@ set -ex
 
 load-kernel-modules.sh
 
+mkdir -p /var/run/stratum
+
 PLATFORM=$(cat /etc/onl/platform)
 
 stratum_bcm \
     -external_stratum_urls=0.0.0.0:28000 \
-    -persistent_config_dir=/tmp/stratum \
+    -persistent_config_dir=/etc/stratum \
     -base_bcm_chassis_map_file=/etc/stratum/stratum_configs/${PLATFORM}/base_bcm_chassis_map.pb.txt \
     -chassis_config_file=/etc/stratum/stratum_configs/${PLATFORM}/chassis_config.pb.txt \
     -bcm_sdk_config_file=/etc/stratum/stratum_configs/${PLATFORM}/SDKLT.yml \
     -phal_config_path=/etc/stratum/stratum_configs/${PLATFORM}/phal_config.pb.txt \
     -bcm_serdes_db_proto_file=/etc/stratum/stratum_configs/dummy_serdes_db.pb.txt \
     -bcm_hardware_specs_file=/etc/stratum/stratum_configs/bcm_hardware_specs.pb.txt \
-    -forwarding_pipeline_configs_file=/tmp/stratum/pipeline_cfg.pb.txt \
-    -write_req_log_file=/tmp/stratum/p4_writes.pb.txt \
+    -forwarding_pipeline_configs_file=/var/run/stratum/pipeline_cfg.pb.txt \
+    -write_req_log_file=/var/run/stratum/p4_writes.pb.txt \
     -bcm_sdk_checkpoint_dir=/tmp/bcm_chkpt \
     -colorlogtostderr \
     -logtosyslog=false \
