a03f3b5cd70a2e28d7222c2bb2f849c6ee0c8bd7
Yi Tseng <yi@opennetworking.org>
2020-04-26 09:38:22 +0800

    fix cpp style

--- stratum/hal/lib/bcm/bcm_sdk_wrapper.h
+++ stratum/hal/lib/bcm/bcm_sdk_wrapper.h
@@ -1,6 +1,6 @@
 // Copyright 2018 Google LLC
 // Copyright 2018-present Open Networking Foundation
-// Copyright 2019 Broadcom. All rights reserved. The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// Copyright 2019 Broadcom. All rights reserved. The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries. NOLINT
 // SPDX-License-Identifier: Apache-2.0
 
 #ifndef STRATUM_HAL_LIB_BCM_BCM_SDK_WRAPPER_H_
