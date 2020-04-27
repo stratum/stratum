40c162ad8cff8ffc782e85e8891e8dfb2b3c79e5
pudelkoM <pudelkoM@users.noreply.github.com>
2020-04-27 10:42:49 -0700

    Post-rebase cleanup

--- stratum/hal/bin/bcm/standalone/sdk/start-stratum.sh
+++ stratum/hal/bin/bcm/standalone/sdk/start-stratum.sh
@@ -30,20 +30,23 @@ insmod linux-kernel-bde.ko && insmod linux-user-bde.ko && insmod linux-bcm-knet.
 popd
 sleep 1
 
+mkdir -p /var/run/stratum /var/log/stratum
+
 PLATFORM=$(cat /etc/onl/platform)
 
-stratum_bcm_opennsa \
+exec stratum_bcm_opennsa \
     -external_stratum_urls=0.0.0.0:28000 \
-    -persistent_config_dir=/tmp/stratum \
+    -persistent_config_dir=/etc/stratum \
     -base_bcm_chassis_map_file=/etc/stratum/stratum_configs/${PLATFORM}/base_bcm_chassis_map.pb.txt \
     -chassis_config_file=/etc/stratum/stratum_configs/${PLATFORM}/chassis_config.pb.txt \
     -bcm_sdk_config_file=/etc/stratum/stratum_configs/${PLATFORM}/config.bcm \
     -phal_config_path=/etc/stratum/stratum_configs/${PLATFORM}/phal_config.pb.txt \
     -bcm_serdes_db_proto_file=/etc/stratum/stratum_configs/dummy_serdes_db.pb.txt \
     -bcm_hardware_specs_file=/etc/stratum/stratum_configs/bcm_hardware_specs.pb.txt \
-    -forwarding_pipeline_configs_file=/tmp/stratum/pipeline_cfg.pb.txt \
-    -write_req_log_file=/tmp/stratum/p4_writes.pb.txt \
+    -forwarding_pipeline_configs_file=/var/run/stratum/pipeline_cfg.pb.txt \
+    -write_req_log_file=/var/log/stratum/p4_writes.pb.txt \
     -bcm_sdk_checkpoint_dir=/tmp/bcm_chkpt \
+    -log_dir=/var/log/stratum \
     -colorlogtostderr \
     -logtosyslog=false \
     -stderrthreshold=0 \
