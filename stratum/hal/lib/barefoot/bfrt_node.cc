44853ac933f416adcc9923d578af3e26e28944ae
Yi Tseng <yi@opennetworking.org>
2020-05-12 15:34:45 +0800

    Initial works
     - main
       - initialize bfrt_node and bfrt_switch
     - bfrt switch
       - use bfrt_node instead of pi_node
     - bfrt node
     - bfrt table manager
       - manage table entry, action profile members

---
+++ stratum/hal/lib/barefoot/bfrt_node.cc
@@ -0,0 +1,293 @@
+// Copyright 2020-present Open Networking Foundation
+// Copyright 2020 Intel/Barefoot Networks
+// SPDX-License-Identifier: Apache-2.0
+
+#include "stratum/hal/lib/barefoot/bfrt_node.h"
+
+#include <memory>
+#include "absl/memory/memory.h"
+#include "bf_rt/bf_rt_init.hpp"
+#include "p4/config/v1/p4info.pb.h"
+#include "stratum/glue/status/status_macros.h"
+#include "stratum/public/proto/error.pb.h"
+#include "stratum/lib/macros.h"
+
+extern "C" {
+#include <unistd.h>
+#include "tofino/bf_pal/dev_intf.h"
+}
+
+namespace stratum {
+namespace hal {
+namespace barefoot {
+BFRuntimeNode::~BFRuntimeNode() = default;
+
+::util::Status BFRuntimeNode::PushChassisConfig(const ChassisConfig& config,
+                                                uint64 node_id) {
+  (void)config;
+  absl::WriterMutexLock l(&lock_);
+  node_id_ = node_id;
+  initialized_ = true;
+
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::VerifyChassisConfig(const ChassisConfig& config,
+                                                  uint64 node_id) {
+  (void)config;
+  (void)node_id;
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::PushForwardingPipelineConfig(
+    const ::p4::v1::ForwardingPipelineConfig& config) {
+  // SaveForwardingPipelineConfig + CommitForwardingPipelineConfig
+  RETURN_IF_ERROR(SaveForwardingPipelineConfig(config));
+  return CommitForwardingPipelineConfig();
+}
+
+::util::Status BFRuntimeNode::SaveForwardingPipelineConfig(
+    const ::p4::v1::ForwardingPipelineConfig& config) {
+  absl::WriterMutexLock l(&lock_);
+  RETURN_IF_ERROR(VerifyForwardingPipelineConfig(config));
+  uint64 cookie = 0;
+  if (config.has_cookie()) {
+    cookie = config.cookie().cookie();
+  }
+  p4::config::v1::P4Info p4info = config.p4info();
+  const char* p4_device_config = config.p4_device_config().c_str();
+
+  // Structure of P4 device config for Barefoot device
+  // {
+  //   prog_name_size: uint32
+  //   prog_name: byte[prog_name__size]
+  //   tofino_bin_size: uint32
+  //   tofino_bin: byte[tofino_bin_size]
+  //   context_json_size: uint32
+  //   context_json: byte[context_json_size]
+  //   bfrt_json_size: uint32
+  //   bf_rt_json: byte[bfrt_json_size]
+  // }
+
+  // From BF-PI implementation
+  const char *device_data_curr = p4_device_config;
+  uint32_t chunk_size;
+  memcpy(&chunk_size, device_data_curr, sizeof(uint32_t));
+  device_data_curr += sizeof(uint32_t);
+  if (chunk_size > _PI_UPDATE_MAX_NAME_SIZE || chunk_size == 0) {
+    RETURN_ERROR(ERR_OUT_OF_RANGE)
+        << "invalid program name size " << chunk_size;
+  }
+  strncpy(prog_name_, device_data_curr, chunk_size);
+  prog_name_[chunk_size] = '\0';
+  device_data_curr += chunk_size;
+
+  // TODO(YI): lots of duplicated code, move to function
+  // Tofino bin
+  memcpy(&chunk_size, device_data_curr, sizeof(uint32_t));
+  device_data_curr += sizeof(uint32_t);
+  snprintf(tofino_bin_path_,
+           _PI_UPDATE_MAX_TMP_FILENAME_SIZE,
+           "%s-tofino.bin.XXXXXX",
+           prog_name_);
+  int cfg_fd = mkstemp(tofino_bin_path_);
+  if (cfg_fd == -1) {
+    RETURN_ERROR()
+        << "error when trying to create temp bin file " << tofino_bin_path_;
+  }
+  ssize_t written = 0;
+  while ((written = write(cfg_fd, device_data_curr, chunk_size)) != -1) {
+    device_data_curr += written;
+    chunk_size -= written;
+    if (chunk_size == 0) break;
+  }
+  if (written == -1) {
+    RETURN_ERROR() << "error when writing bin file " << tofino_bin_path_;
+  }
+
+  // Context JSON file
+  memcpy(&chunk_size, device_data_curr, sizeof(uint32_t));
+  device_data_curr += sizeof(uint32_t);
+  snprintf(ctx_json_path_,
+           _PI_UPDATE_MAX_TMP_FILENAME_SIZE,
+           "%s-ctx.json.XXXXXX",
+           prog_name_);
+  int ctx_fd = mkstemp(ctx_json_path_);
+  if (ctx_fd == -1) {
+    RETURN_ERROR()
+        << "error when trying to create temp context file " << ctx_json_path_;
+  }
+  while ((written = write(ctx_fd, device_data_curr, chunk_size)) != -1) {
+    device_data_curr += written;
+    chunk_size -= written;
+    if (chunk_size == 0) break;
+  }
+  if (written == -1) {
+    RETURN_ERROR()
+        << "error when writing context file " << ctx_json_path_;
+  }
+
+  // BFRuntime JSON file
+  memcpy(&chunk_size, device_data_curr, sizeof(uint32_t));
+  device_data_curr += sizeof(uint32_t);
+  snprintf(bfrt_file_path_,
+           _PI_UPDATE_MAX_TMP_FILENAME_SIZE,
+           "%s-bfrt.json.XXXXXX",
+           prog_name_);
+  int bfrt_fd = mkstemp(bfrt_file_path_);
+  if (bfrt_fd == -1) {
+    RETURN_ERROR()
+        << "error when trying to create temp bfrt file " << bfrt_file_path_;
+  }
+  while ((written = write(bfrt_fd, device_data_curr, chunk_size)) != -1) {
+    device_data_curr += written;
+    chunk_size -= written;
+    if (chunk_size == 0) break;
+  }
+  if (written == -1) {
+    RETURN_ERROR()
+        << "error when writing bfrt file " << ctx_json_path_;
+  }
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::CommitForwardingPipelineConfig() {
+  absl::WriterMutexLock l(&lock_);
+  if (!initialized_) {
+    RETURN_ERROR() << "Not initialized";
+  }
+  auto bf_status = bf_pal_device_warm_init_begin(
+          unit_,
+          BF_DEV_WARM_INIT_FAST_RECFG,
+          BF_DEV_SERDES_UPD_NONE,
+          true);
+  CHECK_RETURN_IF_FALSE(bf_status == BF_SUCCESS)
+      << "Failed to begin warm_init";
+  bf_device_profile_t device_profile;
+
+  // TODO(Yi): Now we only support single P4 program
+  device_profile.num_p4_programs = 1;
+  bf_p4_program_t p4_program = device_profile.p4_programs[0];
+  strncpy(p4_program.prog_name, prog_name_, _PI_UPDATE_MAX_NAME_SIZE);
+  p4_program.bfrt_json_file = bfrt_file_path_;
+  p4_program.num_p4_pipelines = 4;
+
+  // TODO(Yi): Now we applies single pipelines to all HW pipeline
+  bf_p4_pipeline_t pipeline_profile = p4_program.p4_pipelines[0];
+  strcpy(pipeline_profile.p4_pipeline_name, "pipe");
+  pipeline_profile.cfg_file = tofino_bin_path_;
+  pipeline_profile.runtime_context_file = ctx_json_path_;
+
+  // FIXME(YI): do we need to put p4info here?
+  pipeline_profile.pi_config_file = nullptr;
+
+  // Single P4 pipeline for all HW pipeline
+  pipeline_profile.num_pipes_in_scope = 4;
+  // pipe_scope = [0, 1, 2, 3]
+  for (int p = 0; p < 4; ++p) {
+    pipeline_profile.pipe_scope[p] = p;
+  }
+
+  bf_status = bf_pal_device_add(unit_, &device_profile);
+  CHECK_RETURN_IF_FALSE(bf_status == BF_SUCCESS)
+      << "Failed to add device " << unit_;
+
+  bf_status = bf_pal_device_warm_init_end(unit_);
+  CHECK_RETURN_IF_FALSE(bf_status == BF_SUCCESS)
+      << "Failed to finish warn init process.";
+  pipeline_initialized_ = true;
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::VerifyForwardingPipelineConfig(
+      const ::p4::v1::ForwardingPipelineConfig& config) {
+  CHECK_RETURN_IF_FALSE(config.has_p4info()) << "Missing P4 info";
+  CHECK_RETURN_IF_FALSE(!config.p4_device_config().empty())
+      << "Missing P4 device config";
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::Shutdown() {
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::Freeze() {
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::Unfreeze() {
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::WriteForwardingEntries(
+    const ::p4::v1::WriteRequest& req, std::vector<::util::Status>* results) {
+  absl::WriterMutexLock l(&lock_);
+  auto session = bfrt::BfRtSession::sessionCreate();
+  session->beginBatch();
+  ::util::Status status;
+  for (auto update : req.updates()) {
+    switch(update.entity_case()) {
+      case kTableEntry:
+        status = bfrt_tbl_mgr_->WriteTableEntry(session, update.type(), update.table_entry());
+        break;
+      case kExternEntry:
+      case kActionProfileMember:
+      case kActionProfileGroup:
+      case kMeterEntry:
+      case kDirectMeterEntry:
+      case kCounterEntry:
+      case kDirectCounterEntry:
+      case kPacketReplicationEngineEntry:
+      case kValueSetEntry:
+      case kRegisterEntry:
+      case kDigestEntry:
+      default:
+        results->push_back(MAKE_ERROR()
+            << "Unsupported entity type: " << update.ShortDebugString());
+    }
+  }
+  session->endBatch(true);
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::ReadForwardingEntries(const ::p4::v1::ReadRequest& req,
+    WriterInterface<::p4::v1::ReadResponse>* writer,
+    std::vector<::util::Status>* details) {
+  return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::RegisterPacketReceiveWriter(
+    const std::shared_ptr<WriterInterface<::p4::v1::PacketIn>>& writer) {
+return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::UnregisterPacketReceiveWriter() {
+return ::util::OkStatus();
+}
+
+::util::Status BFRuntimeNode::TransmitPacket(const ::p4::v1::PacketOut& packet) {
+return ::util::OkStatus();
+}
+
+// Factory function for creating the instance of the class.
+std::unique_ptr<BFRuntimeNode> BFRuntimeNode::CreateInstance(int unit) {
+  return absl::WrapUnique(new BFRuntimeNode(unit));
+}
+
+BFRuntimeNode::BFRuntimeNode(int unit)
+    : unit_(unit), pipeline_initialized_(false),
+      initialized_(false), node_id_(0) {
+}
+
+void BFRuntimeNode::SendPacketIn(const ::p4::v1::PacketIn& packet) {
+  // acquire the lock during the Write: SendPacketIn may be called from
+  // different threads and Write is not thread-safe.
+  absl::MutexLock l(&rx_writer_lock_);
+  if (rx_writer_ == nullptr) return;
+  rx_writer_->Write(packet);
+}
+
+
+}  // namespace barefoot
+}  // namespace hal
+}  // namespace stratum
---
+++ stratum/hal/lib/barefoot/macros.h
@@ -0,0 +1,84 @@
+// Copyright 2020-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef STRTUM_HAL_LIB_BAREFOOT_MACROS_H
+#define STRTUM_HAL_LIB_BAREFOOT_MACROS_H
+
+extern "C" {
+#include "bf_rt/bf_rt_common.h"
+}
+
+#include "stratum/glue/status/status.h"
+#include "stratum/lib/macros.h"
+#include "stratum/public/lib/error.h"
+
+namespace stratum {
+namespace hal {
+namespace barefoot {
+
+class BooleanBfStatus {
+ public:
+  explicit BooleanBfStatus(bf_status_t status) : status_(status) {}
+  operator bool() const { return status_ == BF_SUCCESS; }
+  inline bf_status_t status() const { return status_; }
+  inline ErrorCode error_code() const {
+    switch(status_) {
+      case BF_SUCCESS:
+        return ERR_SUCCESS;
+      case BF_NOT_READY:
+        return ERR_NOT_INITIALIZED;
+      case BF_INVALID_ARG:
+        return ERR_INVALID_PARAM;
+      case BF_ALREADY_EXISTS:
+        return ERR_ENTRY_EXISTS;
+      case BF_NO_SYS_RESOURCES:
+      case BF_MAX_SESSIONS_EXCEEDED:
+      case BF_NO_SPACE:
+      case BF_EAGAIN:
+        return ERR_NO_RESOURCE;
+      case BF_ENTRY_REFERENCES_EXIST:
+        return ERR_PERMISSION_DENIED;
+      case BF_TXN_NOT_SUPPORTED:
+      case BF_NOT_SUPPORTED:
+        return ERR_OPER_NOT_SUPPORTED;
+      case BF_HW_COMM_FAIL:
+      case BF_HW_UPDATE_FAILED:
+        return ERR_HARDWARE_ERROR;
+      case BF_NO_LEARN_CLIENTS:
+        return ERR_FEATURE_UNAVAILABLE;
+      case BF_IDLE_UPDATE_IN_PROGRESS:
+        return ERR_OPER_STILL_RUNNING;
+      case BF_OBJECT_NOT_FOUND:
+      case BF_TABLE_NOT_FOUND:
+        return ERR_ENTRY_NOT_FOUND;
+      case BF_NOT_IMPLEMENTED:
+        return ERR_UNIMPLEMENTED;
+      case BF_SESSION_NOT_FOUND:
+      case BF_INIT_ERROR:
+      case BF_TABLE_LOCKED:
+      case BF_IO:
+      case BF_UNEXPECTED:
+      case BF_DEVICE_LOCKED:
+      case BF_INTERNAL_ERROR:
+      case BF_IN_USE:
+      default:
+        return ERR_INTERNAL;
+    }
+  }
+
+ private:
+  bf_status_t status_;
+};
+
+#define BFRT_RETURN_IF_ERROR(expr) \
+  if (const BooleanBfStatus __ret = BooleanBfStatus(expr)) { \
+  } else /* NOLINT */ \
+    return MAKE_ERROR(__ret.error_code()) \
+           << "'" << #expr << "' failed with error message: " \
+           << FixMessage(bf_err_str(__ret.status()))
+#endif
+
+}  // namespace barefoot
+}  // namespace hal
+}  // namespace stratum
+
