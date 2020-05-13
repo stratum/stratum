5d798a101d242bce574a14ba575f95442330cd34
pudelkoM <pudelkoM@users.noreply.github.com>
2020-05-13 14:12:33 -0700

    Remove stratum/testing/scripts

--- stratum/testing/scripts/do_nothing.sh
+++
@@ -1,6 +0,0 @@
-# Copyright 2018-present Open Networking Foundation
-# SPDX-License-Identifier: Apache-2.0
-
-# This is a dummy file we pass as the src for sh_test. By using this, we can
-# create a test that passes if all targets in it's data list build.
-exit 0;
