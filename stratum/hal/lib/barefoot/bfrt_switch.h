9651088ed49d26336089e9c00a744be86158cdfa
pudelkoM <pudelkoM@users.noreply.github.com>
2020-06-09 08:45:53 -0700

    Rename BfrtNode

--- stratum/hal/lib/barefoot/bfrt_switch.h
+++ stratum/hal/lib/barefoot/bfrt_switch.h
@@ -6,23 +6,23 @@
 
 #include <map>
 #include <memory>
-#include <vector>
 #include <string>
+#include <vector>
 
+#include "absl/synchronization/mutex.h"
 #include "stratum/hal/lib/barefoot/bf_chassis_manager.h"
 #include "stratum/hal/lib/barefoot/bf_pd_interface.h"
 #include "stratum/hal/lib/barefoot/bfrt_node.h"
 #include "stratum/hal/lib/common/phal_interface.h"
 #include "stratum/hal/lib/common/switch_interface.h"
-#include "absl/synchronization/mutex.h"
 
 namespace stratum {
 namespace hal {
 namespace barefoot {
 
-class BFSwitch : public SwitchInterface {
+class BfrtSwitch : public SwitchInterface {
  public:
-  ~BFSwitch() override;
+  ~BfrtSwitch() override;
 
   // SwitchInterface public methods.
   ::util::Status PushChassisConfig(const ChassisConfig& config) override;
@@ -41,8 +41,8 @@ class BFSwitch : public SwitchInterface {
   ::util::Status Freeze() override;
   ::util::Status Unfreeze() override;
   ::util::Status WriteForwardingEntries(
-       const ::p4::v1::WriteRequest& req,
-       std::vector<::util::Status>* results) override;
+      const ::p4::v1::WriteRequest& req,
+      std::vector<::util::Status>* results) override;
   ::util::Status ReadForwardingEntries(
       const ::p4::v1::ReadRequest& req,
       WriterInterface<::p4::v1::ReadResponse>* writer,
@@ -59,31 +59,30 @@ class BFSwitch : public SwitchInterface {
   ::util::Status RetrieveValue(uint64 node_id, const DataRequest& requests,
                                WriterInterface<DataResponse>* writer,
                                std::vector<::util::Status>* details) override
-        LOCKS_EXCLUDED(chassis_lock);
+      LOCKS_EXCLUDED(chassis_lock);
   ::util::Status SetValue(uint64 node_id, const SetRequest& request,
                           std::vector<::util::Status>* details) override;
   ::util::StatusOr<std::vector<std::string>> VerifyState() override;
 
   // Factory function for creating the instance of the class.
-  static std::unique_ptr<BFSwitch> CreateInstance(
-      PhalInterface* phal_interface,
-      BFChassisManager* bf_chassis_manager,
+  static std::unique_ptr<BfrtSwitch> CreateInstance(
+      PhalInterface* phal_interface, BFChassisManager* bf_chassis_manager,
       BFPdInterface* bf_pd_interface,
       const std::map<int, BfRtNode*>& unit_to_bfrt_node);
 
-  // BFSwitch is neither copyable nor movable.
-  BFSwitch(const BFSwitch&) = delete;
-  BFSwitch& operator=(const BFSwitch&) = delete;
-  BFSwitch(BFSwitch&&) = delete;
-  BFSwitch& operator=(BFSwitch&&) = delete;
+  // BfrtSwitch is neither copyable nor movable.
+  BfrtSwitch(const BfrtSwitch&) = delete;
+  BfrtSwitch& operator=(const BfrtSwitch&) = delete;
+  BfrtSwitch(BfrtSwitch&&) = delete;
+  BfrtSwitch& operator=(BfrtSwitch&&) = delete;
 
  private:
   // Private constructor. Use CreateInstance() to create an instance of this
   // class.
-  BFSwitch(PhalInterface* phal_interface,
-           BFChassisManager* bf_chassis_manager,
-           BFPdInterface* bf_pd_interface,
-           const std::map<int, BfRtNode*>& unit_to_bfrt_node);
+  BfrtSwitch(PhalInterface* phal_interface,
+             BFChassisManager* bf_chassis_manager,
+             BFPdInterface* bf_pd_interface,
+             const std::map<int, BfRtNode*>& unit_to_bfrt_node);
 
   // Helper to get BfRtNode pointer from unit number or return error
   // indicating invalid unit.
