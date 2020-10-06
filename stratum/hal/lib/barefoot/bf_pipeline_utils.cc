c2134ae7eaf20431b9f5f09dfb73ea0e7c7fc5b4
Brian O'Connor <bocon@opennetworking.org>
2020-10-06 18:05:52 -0400

    updating extract to check first

--- stratum/hal/lib/barefoot/bf_pipeline_utils.cc
+++ stratum/hal/lib/barefoot/bf_pipeline_utils.cc
@@ -28,6 +28,19 @@
 
 namespace stratum {
 namespace {
+// Helper function to see in an archive can be read
+::util::Status CheckArchive(const std::string& archive) {
+  struct archive* a = archive_read_new();
+  auto cleanup = gtl::MakeCleanup([&a]() { archive_read_free(a); });
+  archive_read_support_filter_bzip2(a);
+  archive_read_support_filter_gzip(a);
+  archive_read_support_filter_xz(a);
+  archive_read_support_format_tar(a);
+  int r = archive_read_open_memory(a, archive.c_str(), archive.size());
+  CHECK_RETURN_IF_FALSE(r == ARCHIVE_OK) << "Failed to read archive";
+  return util::OkStatus();
+}
+
 // Helper function to extract the contents of first file named filename from
 // an in-memory archive.
 ::util::StatusOr<std::string> ExtractFromArchive(const std::string& archive,
@@ -80,69 +93,71 @@ namespace barefoot {
   }
 
   // Format 2: p4_device_config is an archive of the compiler output.
-  // Find <prog_name>.conf file.
-  nlohmann::json conf;
-  {
-    ASSIGN_OR_RETURN(auto conf_content,
-                     ExtractFromArchive(config.p4_device_config(), ".conf"));
-    conf = nlohmann::json::parse(conf_content, nullptr, false);
-    CHECK_RETURN_IF_FALSE(!conf.is_discarded()) << "Failed to parse .conf";
-    VLOG(1) << ".conf content: " << conf.dump();
-  }
-
-  // Translate JSON conf to protobuf.
-  try {
-    CHECK_RETURN_IF_FALSE(conf["p4_devices"].size() == 1)
-        << "Stratum only supports single devices.";
-    // Only support single devices for now
-    const auto& device = conf["p4_devices"][0];
-    bf_config->set_device(device["device-id"]);
-    for (const auto& program : device["p4_programs"]) {
-      auto p = bf_config->add_programs();
-      // name
-      p->set_name(program["program-name"]);
-      // bfrt.json
-      ASSIGN_OR_RETURN(
-          auto bfrt_content,
-          ExtractFromArchive(config.p4_device_config(), "bfrt.json"));
-      p->set_bfrt(bfrt_content);
-      // p4info.txt
-      *p->mutable_p4info() = config.p4info();
-      ASSIGN_OR_RETURN(
-          auto p4info_content,
-          ExtractFromArchive(config.p4_device_config(), "p4info.txt"));
-      ::p4::config::v1::P4Info p4info_from_tar;
-      CHECK_RETURN_IF_FALSE(google::protobuf::TextFormat::ParseFromString(
-          p4info_content, &p4info_from_tar)) << "Invalid p4info.txt file";
-      CHECK_RETURN_IF_FALSE(ProtoEqual(p4info_from_tar, config.p4info())) <<
-          "P4Info from P4 ForwardingPipelineConfig and archive do not match";
-      // pipes
-      for (const auto& pipeline : program["p4_pipelines"]) {
-        auto pipe = p->add_pipelines();
-        // pipe name
-        pipe->set_name(pipeline["p4_pipeline_name"]);
-        // pipe scope
-        for (const auto& scope : pipeline["pipe_scope"]) {
-          pipe->add_scope(scope);
-        }
-        // pipe context.json
-        ASSIGN_OR_RETURN(
-            const auto context_content,
-            ExtractFromArchive(config.p4_device_config(),
-                               absl::StrCat(pipe->name(), "/context.json")));
-        pipe->set_context(context_content);
-        // pipe tofino.bin
-        ASSIGN_OR_RETURN(
-            const auto config_content,
-            ExtractFromArchive(config.p4_device_config(),
-                               absl::StrCat(pipe->name(), "/tofino.bin")));
-        pipe->set_config(config_content);
-      }
+  if(CheckArchive(config.p4_device_config()).ok()) {
+    // Find <prog_name>.conf file.
+    nlohmann::json conf;
+    {
+        ASSIGN_OR_RETURN(auto conf_content,
+                        ExtractFromArchive(config.p4_device_config(), ".conf"));
+        conf = nlohmann::json::parse(conf_content, nullptr, false);
+        CHECK_RETURN_IF_FALSE(!conf.is_discarded()) << "Failed to parse .conf";
+        VLOG(1) << ".conf content: " << conf.dump();
     }
-    VLOG(2) << bf_config->DebugString();
-    return util::OkStatus();
-  } catch (nlohmann::json::exception& e) {
-    return MAKE_ERROR(ERR_INTERNAL) << e.what();
+
+    // Translate JSON conf to protobuf.
+    try {
+        CHECK_RETURN_IF_FALSE(conf["p4_devices"].size() == 1)
+            << "Stratum only supports single devices.";
+        // Only support single devices for now
+        const auto& device = conf["p4_devices"][0];
+        bf_config->set_device(device["device-id"]);
+        for (const auto& program : device["p4_programs"]) {
+        auto p = bf_config->add_programs();
+        // name
+        p->set_name(program["program-name"]);
+        // bfrt.json
+        ASSIGN_OR_RETURN(
+            auto bfrt_content,
+            ExtractFromArchive(config.p4_device_config(), "bfrt.json"));
+        p->set_bfrt(bfrt_content);
+        // p4info.txt
+        *p->mutable_p4info() = config.p4info();
+        ASSIGN_OR_RETURN(
+            auto p4info_content,
+            ExtractFromArchive(config.p4_device_config(), "p4info.txt"));
+        ::p4::config::v1::P4Info p4info_from_tar;
+        CHECK_RETURN_IF_FALSE(google::protobuf::TextFormat::ParseFromString(
+            p4info_content, &p4info_from_tar)) << "Invalid p4info.txt file";
+        CHECK_RETURN_IF_FALSE(ProtoEqual(p4info_from_tar, config.p4info())) <<
+            "P4Info from P4 ForwardingPipelineConfig and archive do not match";
+        // pipes
+        for (const auto& pipeline : program["p4_pipelines"]) {
+            auto pipe = p->add_pipelines();
+            // pipe name
+            pipe->set_name(pipeline["p4_pipeline_name"]);
+            // pipe scope
+            for (const auto& scope : pipeline["pipe_scope"]) {
+            pipe->add_scope(scope);
+            }
+            // pipe context.json
+            ASSIGN_OR_RETURN(
+                const auto context_content,
+                ExtractFromArchive(config.p4_device_config(),
+                                absl::StrCat(pipe->name(), "/context.json")));
+            pipe->set_context(context_content);
+            // pipe tofino.bin
+            ASSIGN_OR_RETURN(
+                const auto config_content,
+                ExtractFromArchive(config.p4_device_config(),
+                                absl::StrCat(pipe->name(), "/tofino.bin")));
+            pipe->set_config(config_content);
+        }
+        }
+        VLOG(2) << bf_config->DebugString();
+        return util::OkStatus();
+    } catch (nlohmann::json::exception& e) {
+        return MAKE_ERROR(ERR_INTERNAL) << e.what();
+    }
   }
 
   return MAKE_ERROR(ERR_INVALID_PARAM) << "Unknown format for p4_device_config";
