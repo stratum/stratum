6b27ba9522be0962502820b3b7d21b0cf25acfb8
Yi Tseng <yi@opennetworking.org>
2020-10-27 01:50:31 -0700

    Avoid temporal decomposition and shallow function

--- stratum/tools/stratum-replay/stratum_replay.cc
+++ stratum/tools/stratum-replay/stratum_replay.cc
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #include <iostream>
 #include <memory>
-#include <regex>
+#include <regex>  // NOLINT: [build/c++11]
 
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_split.h"
@@ -57,93 +57,13 @@ using ClientStreamChannelReaderWriter =
     ::grpc::ClientReaderWriter<::p4::v1::StreamMessageRequest,
                                ::p4::v1::StreamMessageResponse>;
 
-::util::StatusOr<absl::uint128> GetElectionId() {
-  std::vector<std::string> election_ids =
-      absl::StrSplit(FLAGS_election_id, ",");
-  CHECK_RETURN_IF_FALSE(election_ids.size() == 2) << "Invalid election ID.";
-  uint64 election_id_high;
-  uint64 election_id_low;
-  CHECK_RETURN_IF_FALSE(absl::SimpleAtoi(election_ids[0], &election_id_high))
-      << "Unable to parse string " << election_ids[0] << " to uint64";
-  CHECK_RETURN_IF_FALSE(absl::SimpleAtoi(election_ids[1], &election_id_low))
-      << "Unable to parse string " << election_ids[1] << " to uint64";
-  return absl::MakeUint128(election_id_high, election_id_low);
-}
-
-::util::Status DoAbstractionUpdate(ClientStreamChannelReaderWriter* stream) {
-  ::p4::v1::StreamMessageRequest req;
-  ::p4::v1::StreamMessageResponse resp;
-  ASSIGN_OR_RETURN(auto election_id, GetElectionId());
-  req.mutable_arbitration()->set_device_id(FLAGS_device_id);
-  req.mutable_arbitration()->mutable_election_id()->set_high(
-      absl::Uint128High64(election_id));
-  req.mutable_arbitration()->mutable_election_id()->set_low(
-      absl::Uint128Low64(election_id));
-  if (!stream->Write(req)) {
-    RETURN_ERROR(ERR_INTERNAL) << "Failed to send request '"
-                               << req.ShortDebugString() << "' to switch.";
-  }
-  return ::util::OkStatus();
-}
-
-::util::Status ReplayP4Writes(::grpc::ClientContext* ctx,
-                              ::p4::v1::P4Runtime::Stub* stub,
-                              const std::string& filename) {
-  std::string p4WriteLogs;
-  RETURN_IF_ERROR(::stratum::ReadFileToString(filename, &p4WriteLogs));
-  std::vector<std::string> lines =
-      absl::StrSplit(p4WriteLogs, '\n', absl::SkipEmpty());
-  ASSIGN_OR_RETURN(auto election_id, GetElectionId());
-  ::p4::v1::WriteRequest req;
-  ::p4::v1::WriteResponse resp;
-  int update_prepared = 0;
-  for (std::string line : lines) {
-    // Log format: <timestamp>;<node_id>;<update proto>;<status>
-    // TODO: is it better to use `absl::StrSplit(line, ':')` and
-    // use the third item?
-    std::regex write_req_regex(";(type[^;]*);");
-    std::smatch match;
-    if (!std::regex_search(line, match, write_req_regex)) {
-      // Can not find what we want in this line.
-      VLOG(1) << "Unable to find write request message, skip: " << line;
-      continue;
-    }
-    // Use the first sub-match group
-    std::string write_request_text = match[1].str();
-    auto update = req.add_updates();
-    RETURN_IF_ERROR(ParseProtoFromString(write_request_text, update));
-    ++update_prepared;
-
-    if (update_prepared == FLAGS_write_batch_size) {
-      update_prepared = 0;
-      req.set_device_id(FLAGS_device_id);
-      req.mutable_election_id()->set_high(absl::Uint128High64(election_id));
-      req.mutable_election_id()->set_low(absl::Uint128Low64(election_id));
-
-      VLOG(1) << "Sending request " << req.DebugString();
-      ::grpc::Status status = stub->Write(ctx, req, &resp);
-      CHECK_RETURN_IF_FALSE(status.ok())
-        << "Faild to send P4Runtime write request: "
-        << P4RuntimeGrpcStatusToString(status);
-      req.Clear();
-    }
+::util::Status Main(int argc, char** argv) {
+  if (argc < 2) {
+    LOG(INFO) << kUsage;
+    RETURN_ERROR(ERR_INVALID_PARAM).without_logging() << "";
   }
 
-  if (update_prepared != 0) {
-    req.set_device_id(FLAGS_device_id);
-    req.mutable_election_id()->set_high(absl::Uint128High64(election_id));
-    req.mutable_election_id()->set_low(absl::Uint128Low64(election_id));
-    VLOG(1) << "Sending request " << req.DebugString();
-    ::grpc::Status status = stub->Write(ctx, req, &resp);
-    CHECK_RETURN_IF_FALSE(status.ok())
-        << "Faild to send P4Runtime write request: "
-        << P4RuntimeGrpcStatusToString(status);
-  }
-  return ::util::OkStatus();
-}
-
-::util::StatusOr<std::shared_ptr<::grpc::ChannelCredentials>>
-GetChannelCredentials() {
+  auto channel_credentials = ::grpc::InsecureChannelCredentials();
   if (!FLAGS_ca_cert.empty()) {
     ::grpc::string pem_root_certs;
     ::grpc_impl::experimental::TlsKeyMaterialsConfig::PemKeyCertPair
@@ -167,25 +87,92 @@ GetChannelCredentials() {
         key_materials_config, nullptr, nullptr);
 
     RETURN_IF_ERROR(status);
-    return grpc::experimental::TlsCredentials(cred_opts);
-  } else {
-    return ::grpc::InsecureChannelCredentials();
+    channel_credentials = grpc::experimental::TlsCredentials(cred_opts);
   }
-}
 
-::util::Status Main(int argc, char** argv) {
-  if (argc < 2) {
-    LOG(INFO) << kUsage;
-    RETURN_ERROR(ERR_INVALID_PARAM).without_logging() << "";
-  }
-  ASSIGN_OR_RETURN(auto channel_credentials, GetChannelCredentials());
   auto channel = ::grpc::CreateChannel(FLAGS_grpc_addr, channel_credentials);
   auto stub = ::p4::v1::P4Runtime::NewStub(channel);
   ::grpc::ClientContext context;
   std::unique_ptr<ClientStreamChannelReaderWriter> stream =
       stub->StreamChannel(&context);
-  RETURN_IF_ERROR(DoAbstractionUpdate(stream.get()));
-  return ReplayP4Writes(&context, stub.get(), argv[1]);
+
+  ::p4::v1::StreamMessageRequest stream_req;
+  std::vector<std::string> election_ids =
+      absl::StrSplit(FLAGS_election_id, ",");
+  CHECK_RETURN_IF_FALSE(election_ids.size() == 2) << "Invalid election ID.";
+  uint64 election_id_high;
+  uint64 election_id_low;
+  CHECK_RETURN_IF_FALSE(absl::SimpleAtoi(election_ids[0], &election_id_high))
+      << "Unable to parse string " << election_ids[0] << " to uint64";
+  CHECK_RETURN_IF_FALSE(absl::SimpleAtoi(election_ids[1], &election_id_low))
+      << "Unable to parse string " << election_ids[1] << " to uint64";
+  absl::uint128 election_id =
+      absl::MakeUint128(election_id_high, election_id_low);
+
+  stream_req.mutable_arbitration()->set_device_id(FLAGS_device_id);
+  stream_req.mutable_arbitration()->mutable_election_id()->set_high(
+      absl::Uint128High64(election_id));
+  stream_req.mutable_arbitration()->mutable_election_id()->set_low(
+      absl::Uint128Low64(election_id));
+  if (!stream->Write(stream_req)) {
+    RETURN_ERROR(ERR_INTERNAL)
+        << "Failed to send request '" << stream_req.ShortDebugString()
+        << "' to switch.";
+  }
+
+  std::string p4WriteLogs;
+  RETURN_IF_ERROR(::stratum::ReadFileToString(argv[1], &p4WriteLogs));
+  std::vector<std::string> lines =
+      absl::StrSplit(p4WriteLogs, '\n', absl::SkipEmpty());
+  ::p4::v1::WriteRequest write_req;
+  ::p4::v1::WriteResponse write_resp;
+  int update_prepared = 0;
+  for (std::string line : lines) {
+    // Log format: <timestamp>;<node_id>;<update proto>;<status>
+    // TODO(Yi): Is it better to use `absl::StrSplit(line, ':')` and
+    // use the third item?
+    std::regex write_req_regex(";(type[^;]*);");
+    std::smatch match;
+    if (!std::regex_search(line, match, write_req_regex)) {
+      // Can not find what we want in this line.
+      VLOG(1) << "Unable to find write request message, skip: " << line;
+      continue;
+    }
+    // The regular expression here we are using will put the protobuf text we
+    // need in the first sub-match group.
+    std::string write_request_text = match[1].str();
+    auto update = write_req.add_updates();
+    RETURN_IF_ERROR(ParseProtoFromString(write_request_text, update));
+    ++update_prepared;
+
+    if (update_prepared == FLAGS_write_batch_size) {
+      update_prepared = 0;
+      write_req.set_device_id(FLAGS_device_id);
+      write_req.mutable_election_id()->set_high(
+          absl::Uint128High64(election_id));
+      write_req.mutable_election_id()->set_low(absl::Uint128Low64(election_id));
+
+      VLOG(1) << "Sending request " << write_req.DebugString();
+      ::grpc::Status status = stub->Write(&context, write_req, &write_resp);
+      CHECK_RETURN_IF_FALSE(status.ok())
+          << "Faild to send P4Runtime write request: "
+          << P4RuntimeGrpcStatusToString(status);
+      write_req.Clear();
+    }
+  }
+
+  if (update_prepared != 0) {
+    write_req.set_device_id(FLAGS_device_id);
+    write_req.mutable_election_id()->set_high(absl::Uint128High64(election_id));
+    write_req.mutable_election_id()->set_low(absl::Uint128Low64(election_id));
+    VLOG(1) << "Sending request " << write_req.DebugString();
+    ::grpc::Status status = stub->Write(&context, write_req, &write_resp);
+    CHECK_RETURN_IF_FALSE(status.ok())
+        << "Faild to send P4Runtime write request: "
+        << P4RuntimeGrpcStatusToString(status);
+  }
+
+  return ::util::OkStatus();
 }
 
 }  // namespace p4rt_replay
