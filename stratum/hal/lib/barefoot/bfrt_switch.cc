fc36490bfd7187eb7ebb98fe3ed7e727cca06b3c
Brian O'Connor <bocon@opennetworking.org>
2020-11-10 18:23:34 -0500

    Update stratum/hal/lib/barefoot/bfrt_switch.cc

--- stratum/hal/lib/barefoot/bfrt_switch.cc
+++ stratum/hal/lib/barefoot/bfrt_switch.cc
@@ -205,7 +205,7 @@ BfrtSwitch::~BfrtSwitch() {}
       case DataRequest::Request::kAutonegStatus:
       case DataRequest::Request::kFrontPanelPortInfo:
       case DataRequest::Request::kLoopbackStatus:
-      case DataRequest::Request::kSdkPortId: {
+      case DataRequest::Request::kSdnPortId: {
         auto port_data = bf_chassis_manager_->GetPortData(req);
         if (!port_data.ok()) {
           status.Update(port_data.status());
