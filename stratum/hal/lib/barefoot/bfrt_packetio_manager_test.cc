82eb3f9288bb06816a472abf68e373075d4e6790
pudelkoM <pudelkoM@users.noreply.github.com>
2020-12-01 10:39:41 -0800

    Remove SDE dep from packetio manager

--- stratum/hal/lib/barefoot/bfrt_packetio_manager_test.cc
+++ stratum/hal/lib/barefoot/bfrt_packetio_manager_test.cc
@@ -7,6 +7,7 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "stratum/glue/status/status_test_util.h"
+#include "stratum/hal/lib/barefoot/bf_sde_mock.h"
 #include "stratum/lib/utils.h"
 
 using ::testing::_;
@@ -23,20 +24,23 @@ namespace barefoot {
 class BfrtPacketioManagerTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    bfrt_packetio_manager_ = BfrtPacketioManager::CreateInstance(kDevice1);
+    bf_sde_wrapper_mock_ = absl::make_unique<BfSdeMock>();
+    bfrt_packetio_manager_ = BfrtPacketioManager::CreateInstance(
+        kDevice1, bf_sde_wrapper_mock_.get());
   }
 
   static constexpr int kDevice1 = 0;
 
+  std::unique_ptr<BfSdeMock> bf_sde_wrapper_mock_;
   std::unique_ptr<BfrtPacketioManager> bfrt_packetio_manager_;
 };
 
 constexpr int BfrtPacketioManagerTest::kDevice1;
 
 TEST_F(BfrtPacketioManagerTest, TransmitPacketAfterChassisConfigPush) {
-
+  // TODO
+  EXPECT_TRUE(false);
 }
-
 
 }  // namespace barefoot
 }  // namespace hal
