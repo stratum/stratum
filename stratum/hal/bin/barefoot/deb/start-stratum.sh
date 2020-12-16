8bf39b3707ec0641e9931058e3be5a936e484171
Maximilian Pudelko <pudelkoM@users.noreply.github.com>
2020-12-16 10:36:26 -0800

    Automatically allocate hugepages in BF start-stratum.sh (#279)
    Co-authored-by: Brian O'Connor <bocon@opennetworking.org>

--- stratum/hal/bin/barefoot/deb/start-stratum.sh
+++ stratum/hal/bin/barefoot/deb/start-stratum.sh
@@ -29,6 +29,34 @@ if [ ! -f "$FLAG_FILE" ]; then
     exit 255
 fi
 
+# Check number of hugepages available (and set up if not configured)
+nr_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
+free_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/free_hugepages)
+# The number of pages needed is dynamically computed by bf_switchd.
+# stratum_bf uses around 70 pages in testing, but 128 is a safe bound.
+if [[ $nr_hugepages -lt 128 ]] || [[ $free_hugepages -lt 128 ]]; then
+    if [[ $nr_hugepages -eq 0 ]]; then
+        echo "Setting up hugepages..."
+        if ! echo 128 > /proc/sys/vm/nr_hugepages; then
+            echo "Failed to set up hugepages."
+            exit 255
+        fi
+    else
+        echo "ERROR: There are $free_hugepages free hugepages, and 128 are required."
+        echo "       The system has $nr_hugepages hugepages allocated in total."
+        exit 255
+    fi
+fi
+
+# Mount hugepages fs if necessary
+if ! grep hugetlbfs /etc/mtab | grep -q /mnt/huge; then
+    echo "Mounting hugepages..."
+    if ! ( mkdir -p /mnt/huge && mount -t hugetlbfs nodev /mnt/huge ); then
+        echo "Failed to mount hugepages."
+        exit 255
+    fi
+fi
+
 # Set up port map for device
 PORT_MAP="/etc/stratum/$PLATFORM/port_map.json"
 if [ ! -f "$PORT_MAP" ]; then
