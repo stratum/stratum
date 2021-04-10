c8bc123d8cdf214a1e511b2085d9af326dc3e28e
Brian O'Connor <bocon@opennetworking.org>
2021-04-10 04:19:03 +0000

    wip bf interface

---
+++ stratum/lib/barefoot/bf_interface.cc
@@ -0,0 +1,122 @@
+// Copyright 2021-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "stratum/lib/barefoot/bf_interface.h"
+
+namespace stratum {
+namespace barefoot {
+
+class BfInterfaceImpl : public BfInterface {
+ public:
+//   explicit TableKey(std::unique_ptr<bfrt::BfRtTableKey> table_key)
+//       : table_key_(std::move(table_key)) {}
+
+  ::absl::Status InitSde() override;
+  ::absl::Status SetForwardingPipelineConfig(
+      const ::p4::v1::SetForwardingPipelineConfigRequest& req,
+      ::p4::v1::SetForwardingPipelineConfigResponse* resp) override;
+  ::absl::Status GetForwardingPipelineConfig(
+      const ::p4::v1::GetForwardingPipelineConfigRequest& req,
+      ::p4::v1::GetForwardingPipelineConfigResponse* resp) override;
+  ::absl::Status Write(const ::p4::v1::WriteRequest& req,
+                        ::p4::v1::WriteResponse* resp) override;  
+  ::absl::Status Read(const ::p4::v1::ReadRequest& req,
+                       ::p4::v1::ReadResponse* resp) override;
+};
+
+::absl::Status BfInterfaceImpl::InitSde() {
+  return absl::OkStatus();
+}
+
+::absl::Status BfInterfaceImpl::SetForwardingPipelineConfig(
+    const ::p4::v1::SetForwardingPipelineConfigRequest& req,
+    ::p4::v1::SetForwardingPipelineConfigResponse* resp) {
+  return absl::OkStatus();
+}
+
+::absl::Status BfInterfaceImpl::GetForwardingPipelineConfig(
+    const ::p4::v1::GetForwardingPipelineConfigRequest& req,
+    ::p4::v1::GetForwardingPipelineConfigResponse* resp) {
+  return absl::OkStatus();
+}
+
+::absl::Status BfInterfaceImpl::Write(const ::p4::v1::WriteRequest& req,
+                    ::p4::v1::WriteResponse* resp) {
+  return absl::OkStatus();
+}  
+
+::absl::Status BfInterfaceImpl::Read(const ::p4::v1::ReadRequest& req,
+                    ::p4::v1::ReadResponse* resp) {
+  return absl::OkStatus();              
+}
+
+
+}  // namespace barefoot
+}  // namespace stratum
+
+
+namespace {
+
+static ::stratum::barefoot::BfInterfaceImpl* bf_interface_;
+
+// A macro for simplify checking of arguments in the C functions.
+#define CHECK_RETURN_IF_FALSE(cond) \
+  if (ABSL_PREDICT_TRUE(cond)) {    \
+  } else /* NOLINT */               \
+    return static_cast<int>(::absl::StatusCode::kInvalidArgument)
+
+// A macro that converts an absl::Status to an int and returns it.
+#define RETURN_STATUS(status) return static_cast<int>(status.code())
+
+// TODO(bocon): consider free if response not null
+#define RETURN_CPP_API(RequestProto, ResponseProto, Function)          \
+  CHECK_RETURN_IF_FALSE(packed_response == NULL);                      \
+  RequestProto request;                                                \
+  CHECK_RETURN_IF_FALSE(                                               \
+      request.ParseFromArray(packed_request, request_size));           \
+  ResponseProto response;                                              \
+  ::absl::Status status = bf_interface_->Function(request, &response); \
+  response_size = response.ByteSizeLong();                             \
+  packed_response = malloc(response_size);                             \
+  response.SerializeToArray(packed_response, response_size);           \
+  RETURN_STATUS(status)
+
+}  // namespace
+
+// TODO(bocon): Consider templatizing the C methods
+
+int bf_p4_set_pipeline_config(const PackedProtobuf packed_request,
+                               size_t request_size,
+                               PackedProtobuf& packed_response,
+                               size_t& response_size) {
+  RETURN_CPP_API(
+    ::p4::v1::SetForwardingPipelineConfigRequest,
+    ::p4::v1::SetForwardingPipelineConfigResponse,
+    SetForwardingPipelineConfig);
+}
+
+int bf_p4_get_pipeline_config(const PackedProtobuf packed_request,
+                               size_t request_size,
+                               PackedProtobuf& packed_response,
+                               size_t& response_size) {
+  RETURN_CPP_API(
+    ::p4::v1::GetForwardingPipelineConfigRequest,
+    ::p4::v1::GetForwardingPipelineConfigResponse,
+    GetForwardingPipelineConfig);
+}
+
+int bf_p4_write(const PackedProtobuf packed_request, size_t request_size,
+                PackedProtobuf& packed_response, size_t& response_size) {
+  RETURN_CPP_API(
+    ::p4::v1::WriteRequest,
+    ::p4::v1::WriteResponse,
+    Write);
+}
+
+int bf_p4_read(const PackedProtobuf packed_request, size_t request_size,
+               PackedProtobuf& packed_response, size_t& response_size) {
+  RETURN_CPP_API(
+    ::p4::v1::ReadRequest,
+    ::p4::v1::ReadResponse,
+    Read);
+}
---
+++ stratum/lib/barefoot/bf_interface.h
@@ -0,0 +1,101 @@
+// Copyright 2021-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef STRATUM_LIB_BAREFOOT_BF_INTERFACE_H_
+#define STRATUM_LIB_BAREFOOT_BF_INTERFACE_H_
+
+// Define BfInterface C++ class.
+
+#ifdef  __cplusplus
+
+#include <vector>
+
+#include "absl/status/status.h"
+#include "p4/v1/p4runtime.pb.h"
+
+namespace stratum {
+namespace barefoot {
+
+// TODO The "BfSdeInterface" class in HAL implements a shim layer around the Barefoot
+class BfInterface {
+ public:
+
+  virtual ::absl::Status InitSde() = 0;
+
+  // Pushes the P4-based forwarding pipeline configuration of one or more
+  // switching nodes.
+  virtual ::absl::Status SetForwardingPipelineConfig(
+      const ::p4::v1::SetForwardingPipelineConfigRequest& req,
+      ::p4::v1::SetForwardingPipelineConfigResponse* resp) = 0;
+
+  // Gets the P4-based forwarding pipeline configuration of one or more
+  // switching nodes previously pushed to the switch.
+  virtual ::absl::Status GetForwardingPipelineConfig(
+      const ::p4::v1::GetForwardingPipelineConfigRequest& req,
+      ::p4::v1::GetForwardingPipelineConfigResponse* resp) = 0;
+
+  // Writes one or more forwarding entries on the target as part of P4Runtime
+  // API. Entries include tables entries, action profile members/groups, meter
+  // entries, and counter entries.
+  virtual ::absl::Status Write(const ::p4::v1::WriteRequest& req,
+                               ::p4::v1::WriteResponse* resp) = 0;  
+
+  // Reads the forwarding entries that have been previously written on the
+  // target as part of P4Runtime API.
+  virtual ::absl::Status Read(const ::p4::v1::ReadRequest& req,
+                              ::p4::v1::ReadResponse* resp) = 0;
+
+  // Bidirectional channel between controller and the switch for packet I/O,
+  // master arbitration and stream errors.
+//   virtual ::absl::Status StreamChannel(
+//       ::grpc::ServerContext* context,
+//       ServerStreamChannelReaderWriter* stream) override;
+
+
+};
+
+}  // namespace barefoot
+}  // namespace stratum
+
+#endif  //  __cplusplus
+
+// Define C functions to access BfInterface C++ class.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <cstddef>
+
+typedef void* PackedProtobuf;
+
+int bf_init();
+
+int bf_destroy();
+
+int bf_p4_set_pipeline_config(const PackedProtobuf packed_request,
+                               size_t request_size,
+                               PackedProtobuf& packed_response,
+                               size_t& response_size);
+
+int bf_p4_get_pipeline_config(const PackedProtobuf packed_request,
+                              size_t request_size,
+                              PackedProtobuf& packed_response,
+                              size_t& response_size);
+
+
+int bf_p4_write(const PackedProtobuf packed_write_request,
+                size_t request_size,
+                PackedProtobuf& packed_read_response,
+                size_t& response_size);
+
+int bf_p4_read(const PackedProtobuf packed_read_request,
+               size_t request_size,
+               PackedProtobuf& packed_read_response,
+               size_t& response_size);
+
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // STRATUM_LIB_BAREFOOT_BF_INTERFACE_H_
