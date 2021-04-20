0890b436a4e03b18b5a6d22f68157c5fceeef758
renner <renner@sunder.io>
2021-04-20 14:41:45 -0400

    Add stratum tna (#630)
    This PR adds a P4 pipeline for the Tofino native architecture (TNA). It's goal is usage in integration tests and as an example to get new users started with something™️.

---
+++ stratum/pipelines/stratum-tna/README.md
@@ -0,0 +1,20 @@
+<!--
+Copyright 2021-present Open Networking Foundation
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+# Stratum_tna P4 pipeline
+
+Stratum_tna is an example pipeline for Tofino-enabled switches. It is used for
+testing and demonstration purposes.
+
+## Building
+
+- Set `SDE_INSTALL` env variable
+- `./build.sh`
+
+## Testing
+
+- `cd stratum/pipelines/stratum-tna/; ./build.sh; prevd`
+- `bazel run //stratum/pipelines/stratum-tna:stratum_tna_test -- --grpc_addr=<switch_address> --p4_info_file=$(pwd)/stratum/pipelines/stratum-tna/build/p4info.pb.txt --p4_pipeline_config_file=$(pwd)/stratum/pipelines/stratum-tna/build/device_config.pb.bin`
---
+++ stratum/pipelines/stratum-tna/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Copyright 2021-present Open Networking Foundation
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+if [[ -z "$SDE_INSTALL" ]]; then
+    echo "SDE_INSTALL must be set!"
+    exit 1
+fi
+
+BF_P4C="$SDE_INSTALL/bin/bf-p4c"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+P4_SRC_DIR=${DIR}
+output_dir="${DIR}/build/"
+p4c_flags=""
+OTHER_PP_FLAGS=$2
+
+$BF_P4C --arch tna -g --create-graphs --verbose 2 \
+      -o ${output_dir} -I ${P4_SRC_DIR} \
+      ${OTHER_PP_FLAGS} \
+      ${p4c_flags} \
+      --p4runtime-files ${output_dir}/p4info.pb.txt \
+      --p4runtime-force-std-externs \
+      ${DIR}/stratum_tna.p4
+
+# Generate the pipeline config binary.
+bazel run //stratum/hal/bin/barefoot:bf_pipeline_builder -- \
+    -p4c_conf_file=${output_dir}/stratum_tna.conf \
+    -bf_pipeline_config_binary_file=${output_dir}/device_config.pb.bin
+
+echo "Built pipeline: ${output_dir}/device_config.pb.bin"
