a3ef86f313bd3841d02e08a8eb1ecf5dbdf4963b
Maximilian Pudelko <pudelkoM@users.noreply.github.com>
2021-05-04 07:19:10 -0700

    Relax locking in BfrtNode for concurrent Reads and StreamMessageRequests (#666)
    This change allows concurrent handling of P4RT reads and StreamMessageRequests (e.g. PacketOuts). Long-running read requests no longer block handling of PacketOuts, which can confuse controllers.

--- stratum/hal/lib/barefoot/bfrt_node.cc
+++ stratum/hal/lib/barefoot/bfrt_node.cc
@@ -260,7 +260,7 @@ std::unique_ptr<BfrtNode> BfrtNode::CreateInstance(
     const ::p4::v1::ReadRequest& req,
     WriterInterface<::p4::v1::ReadResponse>* writer,
     std::vector<::util::Status>* details) {
-  absl::WriterMutexLock l(&lock_);
+  absl::ReaderMutexLock l(&lock_);
   CHECK_RETURN_IF_FALSE(req.device_id() == node_id_)
       << "Request device id must be same as id of this BfrtNode.";
   if (!initialized_ || !pipeline_initialized_) {
@@ -385,7 +385,7 @@ std::unique_ptr<BfrtNode> BfrtNode::CreateInstance(
 
 ::util::Status BfrtNode::HandleStreamMessageRequest(
     const ::p4::v1::StreamMessageRequest& req) {
-  absl::WriterMutexLock l(&lock_);
+  absl::ReaderMutexLock l(&lock_);
   if (!initialized_) {
     return MAKE_ERROR(ERR_NOT_INITIALIZED) << "Not initialized!";
   }
