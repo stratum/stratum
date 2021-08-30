f715051bb412e69e372d9677ea5369ccfd7a054f
Maximilian Pudelko <pudelkoM@users.noreply.github.com>
2021-08-30 14:32:24 -0700

    Remove Google-specific `GOOGLE_BASE_HAS_INITGOOGLE` and `STLStringResizeUninitialized` (#819)
    The define causes compiler warnings and serves no purpose anymore. `STLStringResizeUninitialized` is removed, as there are no usages anywhere. Should it be required at a later point, we can fall back to Abseils internal version of it: https://github.com/abseil/abseil-cpp/blob/aad2c8a3966424bbaa2f26027d104d17f9c1c1ae/absl/strings/internal/resize_uninitialized.h#L61-L68

--- stratum/glue/init_google.h
+++ stratum/glue/init_google.h
@@ -5,20 +5,9 @@
 #ifndef STRATUM_GLUE_INIT_GOOGLE_H_
 #define STRATUM_GLUE_INIT_GOOGLE_H_
 
-// TODO(boc): google only: this is not required for open source
-// #include "base/init_google.h"  // IWYU pragma: export
-
-// InitGoogle is not defined in portable base, so we instead emulate its
-// behavior.
-#if !GOOGLE_BASE_HAS_INITGOOGLE
-
 #include "gflags/gflags.h"
 #include "stratum/glue/logging.h"
 #include "stratum/glue/stamping.h"
-
-// TODO(unknown) need to define transformation or comment this out on
-// Google's side
-using gflags::ParseCommandLineFlags;
 
 inline void InitGoogle(const char* usage, int* argc, char*** argv,
                        bool remove_flags) {
@@ -38,9 +27,7 @@ inline void InitGoogle(const char* usage, int* argc, char*** argv,
                                                 ::gflags::SET_FLAGS_DEFAULT)
              .empty());
   ::gflags::SetVersionString(stratum::kBuildScmRevision);
-  ParseCommandLineFlags(argc, argv, remove_flags);
+  ::gflags::ParseCommandLineFlags(argc, argv, remove_flags);
 }
-
-#endif  // !GOOGLE_BASE_HAS_INITGOOGLE
 
 #endif  // STRATUM_GLUE_INIT_GOOGLE_H_
