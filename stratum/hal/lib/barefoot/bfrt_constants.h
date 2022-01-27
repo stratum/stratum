1979c975e322bff9377a8ace2d48b41f917dc6db
Yi Tseng <yi@opennetworking.org>
2022-01-26 23:44:54 -0800

    Cleanup

--- stratum/hal/lib/barefoot/bfrt_constants.h
+++ stratum/hal/lib/barefoot/bfrt_constants.h
@@ -4,9 +4,6 @@
 #ifndef STRATUM_HAL_LIB_BAREFOOT_BFRT_CONSTANTS_H_
 #define STRATUM_HAL_LIB_BAREFOOT_BFRT_CONSTANTS_H_
 
-#include <string>
-
-#include "absl/container/flat_hash_map.h"
 #include "absl/time/time.h"
 #include "stratum/glue/integral_types.h"
 
