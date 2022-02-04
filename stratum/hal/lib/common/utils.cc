53abae1c03c082c5522b648698b897089a3ade4f
pudelkoM <pudelkoM@users.noreply.github.com>
2022-02-04 14:40:11 -0800

    fixup

--- stratum/hal/lib/common/utils.cc
+++ stratum/hal/lib/common/utils.cc
@@ -431,6 +431,8 @@ std::string ConvertHwStateToPresentString(const HwState& hw_state) {
 
 uint64 ConvertHzToMHz(const uint64& val) { return val / 1000000; }
 
+uint64 ConvertMHzToHz(const uint64& val) { return val * 1000000; }
+
 ::util::Status ConvertStringToLogSeverity(const std::string& severity_string,
                                           LoggingConfig* logging_config) {
   if (severity_string == "CRITICAL") {
