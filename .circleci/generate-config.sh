9345694d1928bc6ad1814766f8f95cf7b038e73a
Tseng, Yi <yi.tseng@intel.com>
2022-07-19 23:13:45 +0000

    Find PR number and get PR base from GH API

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Copyright 2022-present Intel
 # SPDX-License-Identifier: Apache-2.0
+
 STRATUM_ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/..; pwd)
 STRATUM_BUILDER_IMAGE="stratumproject/build:build"
-
+PR_NUMBER=$(echo "$CIRCLE_PULL_REQUEST" | grep -Eo '([0-9]+)')
+BASE_REVISION=$(curl -o- -L "https://api.github.com/repos/stratum/stratum/pulls/$PR_NUMBER" | jq -r .base.ref)
 DIFF_FILE=$(mktemp)
 git diff "$BASE_REVISION" HEAD "$STRATUM_ROOT/Dockerfile.build" > "$DIFF_FILE"
 
 if [[ -s "$DIFF_FILE" ]]; then
-    # CIRCLE_PULL_REQUEST is the link of the pull request which will be
-    # unique for each PR.
-    PR_SHA=$(echo $CIRCLE_PULL_REQUEST | sha256sum | awk '{print $1}')
-    STRATUM_BUILDER_IMAGE="stratumproject/build-ci:$PR_SHA"
-fi            
+    STRATUM_BUILDER_IMAGE="stratumproject/build-ci:pr-$PR_NUMBER"
+fi
 
 sed "s#STRATUM_BUILDER_IMAGE#$STRATUM_BUILDER_IMAGE#g" "$STRATUM_ROOT/.circleci/config_template.yml"
