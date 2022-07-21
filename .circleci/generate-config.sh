492a70070391844a9fe6947ca85caf738451d398
Tseng, Yi <yi.tseng@intel.com>
2022-07-21 22:08:23 +0000

    Use script to generate config

---
+++ .circleci/generate-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright 2022-present Intel
+# SPDX-License-Identifier: Apache-2.0
+
+STRATUM_ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/..; pwd)
+STRATUM_BUILDER_IMAGE="stratumproject/build:build"
+
+if [[ "$CIRCLE_BRANCH" != "main" ]]; then
+    DOCKERFILE_SHA=$(sha256sum "$STRATUM_ROOT/Dockerfile.build" | awk '{print $1}')
+    STRATUM_BUILDER_IMAGE="stratumproject/build-ci:$DOCKERFILE_SHA"
+fi
+
+sed "s#STRATUM_BUILDER_IMAGE#$STRATUM_BUILDER_IMAGE#g" "$STRATUM_ROOT/.circleci/config_template.yml"
