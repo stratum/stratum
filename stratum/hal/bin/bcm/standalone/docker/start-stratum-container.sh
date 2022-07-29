f1fee6edf1c7f5841ae7896d438f8f8f5d3669bd
Yi Tseng <a86487817@gmail.com>
2022-07-29 15:32:33 -0700

    Use SONiC as Stratum base OS (#932)
    This PR changes the Stratum base OS from ONL to SONiC.
    As part of the update Debian stretch (9) will be updated to buster (10).
    The default kernel version is now 4.19.0.
    Running Stratum on ONL remains possible, but will require user changes to the startup scripts.

--- stratum/hal/bin/bcm/standalone/docker/start-stratum-container.sh
+++ stratum/hal/bin/bcm/standalone/docker/start-stratum-container.sh
@@ -4,13 +4,31 @@
 
 set -ex
 
-ONLP_MOUNT=$(ls /lib/**/libonlp* | awk '{print "-v " $1 ":" $1 " " }')
-
 LOG_DIR=${LOG_DIR:-/var/log/stratum}
 KERNEL_VERSION=$(uname -r)
 DOCKER_IMAGE=${DOCKER_IMAGE:-stratumproject/stratum-bcm}
 DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG:-sdklt}
-PLATFORM=$(cat /etc/onl/platform)
+
+# Try to load the platform string if not already set.
+if [[ -z "$PLATFORM" ]] && [[ -f "/etc/onl/platform" ]]; then
+    PLATFORM=$(cat /etc/onl/platform)
+elif [[ -z "$PLATFORM" ]] && [[ -f "/etc/sonic/sonic-environment" ]]; then
+    PLATFORM=$(source /etc/sonic/sonic-environment; echo "$PLATFORM" | sed 's/_/-/g')
+    echo "Stopping SONiC services..."
+    sudo systemctl stop sonic.target
+elif [[ -z "$PLATFORM" ]]; then
+    echo "PLATFORM variable must be set manually on non-ONL switches."
+    exit 255
+fi
+
+# Mount ONL related directories, if they exist.
+if [ -d "/etc/onl" ]; then
+    # Use ONLP to find platform and its library
+    ONLP_MOUNT=$(ls /lib/**/libonlp* | awk '{print "-v " $1 ":" $1 " " }')
+    ONLP_MOUNT="$ONLP_MOUNT \
+              -v /lib/platform-config:/lib/platform-config \
+              -v /etc/onl:/etc/onl"
+fi
 
 if [ -n "$CHASSIS_CONFIG" ]; then
     CHASSIS_CONFIG_MOUNT="-v $CHASSIS_CONFIG:/etc/stratum/$PLATFORM/chassis_config.pb.txt"
@@ -22,9 +40,9 @@ fi
 docker run -it --rm --privileged --cap-add ALL --shm-size=512m --network host \
     -v /dev:/dev -v /sys:/sys -v /run:/run \
     -v /lib/modules/$(uname -r):/lib/modules/$(uname -r) \
-    $ONLP_MOUNT \
     -v /lib/platform-config:/lib/platform-config \
-    -v /etc/onl:/etc/onl \
+    --env PLATFORM=$PLATFORM \
+    $ONLP_MOUNT \
     $CHASSIS_CONFIG_MOUNT \
     -v $LOG_DIR:/var/log/stratum \
     $DOCKER_IMAGE:$DOCKER_IMAGE_TAG \
