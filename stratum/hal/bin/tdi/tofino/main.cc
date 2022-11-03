0d8932b205cac092d0da80da07685057df6cdc24
pudelkoM <pudelkoM@users.noreply.github.com>
2022-11-03 11:21:30 -0700

    Ignore TDI tofino for now

--- stratum/hal/bin/tdi/tofino/main.cc
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
-#include "stratum/hal/bin/tdi/tofino/tofino_main.h"
-
-int main(int argc, char* argv[]) {
-  return stratum::hal::tdi::TofinoMain(argc, argv).error_code();
-}
