b25aed53e642448de87403d91871eed254287cca
pudelkoM <pudelkoM@users.noreply.github.com>
2022-11-03 12:56:28 -0700

    Move and fix build scripts

--- stratum/hal/bin/tdi/dpdk/build-p4-sde.sh
+++ stratum/hal/bin/tdi/dpdk/build-p4-sde.sh
@@ -3,31 +3,23 @@
 # SPDX-License-Identifier: Apache-2.0
 set -ex
 
-DOCKERFILE_DIR=$( cd $(dirname "${BASH_SOURCE[0]}") >/dev/null 2>&1 && pwd )
-STRATUM_ROOT=${STRATUM_ROOT:-"$( cd "$DOCKERFILE_DIR/" >/dev/null 2>&1 && pwd )"}
+THIS_DIR=$( cd $(dirname "${BASH_SOURCE[0]}") >/dev/null 2>&1 && pwd )
+STRATUM_ROOT=${STRATUM_ROOT:-"$( cd "$THIS_DIR/../../../../.." >/dev/null 2>&1 && pwd )"}
 JOBS=${JOBS:-4}
 
 print_help() {
 echo "
-The script builds containerized version of Stratum for Barefoot Tofino based device.
-It also builds the kernel module if kernel header tarball is given.
-Usage: $0 [SDE_TAR [KERNEL_HEADERS_TAR]...]
+The script builds the P4 DPDK backend and creates a tar archive for later
+consumption in Stratum.
+Usage: $0
 
 Example:
-    $0 ~/bf-sde-9.7.0.tgz
-    $0 ~/bf-sde-9.7.0.tgz ~/linux-4.14.49-ONL.tar.xz
-    SDE_INSTALL_TAR=~/bf-sde-9.7.0-install.tgz $0
+    $0
+    JOBS=16 $0
 
 Additional environment variables:
-    SDE_INSTALL_TAR: Tar archive of BF SDE install (set to skip SDE build)
-    SDE_INSTALL: Path to BF SDE install directory (set to skip SDE build)
-    STRATUM_TARGET: stratum_bfrt (Default: stratum_bfrt)
-    STRATUM_ROOT: The root directory of Stratum.
     JOBS: The number of jobs to run simultaneously while building the base container. (Default: 4)
-    DOCKER_IMG: Docker image to use for building (Default: stratumproject/build:build)
-    RELEASE_BUILD: Optimized build with stripped symbols (Default: false)
-    BAZEL_CACHE: Path to Bazel cache (Default: <empty>)
-    BSP: Path to optional BSP package directory (Default: <empty>)
+    STRATUM_ROOT: The root directory of Stratum.
 "
 }
 
