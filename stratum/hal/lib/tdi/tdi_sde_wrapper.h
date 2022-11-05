2716057771312e126250ba55175cb1621ef1cb07
pudelkoM <pudelkoM@users.noreply.github.com>
2022-11-04 17:20:30 -0700

    common.proto cleanup

--- stratum/hal/lib/tdi/tdi_sde_wrapper.h
+++ stratum/hal/lib/tdi/tdi_sde_wrapper.h
@@ -164,8 +164,6 @@ class TdiSdeWrapper : public TdiSdeInterface {
   ::util::Status AddPort(int device, int port, uint64 speed_bps,
                          const PortConfigParams& config,
                          FecMode fec_mode) override;
-  ::util::Status HotplugPort(int device, int port,
-                             HotplugConfigParams& hotplug_config) override;
   ::util::Status DeletePort(int device, int port) override;
   ::util::Status EnablePort(int device, int port) override;
   ::util::Status DisablePort(int device, int port) override;
